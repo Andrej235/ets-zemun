96791ca7cd87e1b0d836b12433ef087083b2b3ff
Andrej235 <andrej.nenadic06@gmail.com>
2024-11-27 23:49:30 +0100

    Configure vite's preprocessor to include an @use statement for a color palette in every scss file

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@styles": path.resolve(__dirname, "public/sass"),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@styles/_palette.scss" as *;\n`,
+      },
+    },
+  },
+});
