7e5b85b243c72c689ae8ed8b5a953a894e596d38
Andrej235 <andrej.nenadic06@gmail.com>
2024-11-29 22:31:50 +0100

    Add link to the header

--- src/Components/AppHeader/AppHeader.tsx
+++ src/Components/AppHeader/AppHeader.tsx
@@ -1,5 +1,16 @@
+import { Link } from "react-router";
 import "./AppHeader.scss";
 
 export default function AppHeader() {
-  return <div id="app-header">Header</div>;
+  return (
+    <div id="app-header">
+      <Link to="/">
+        <img src="./logo.png" alt="Logo" />
+      </Link>
+
+      <Link to="/news">News</Link>
+      <Link to="/info">Info</Link>
+      <Link to="/profiles">Profiles</Link>
+    </div>
+  );
 }
