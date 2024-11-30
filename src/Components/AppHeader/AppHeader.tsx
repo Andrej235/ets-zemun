28a0cd868187fa3c27899d3ae264dc54fb20bae2
Andrej235 <andrej.nenadic06@gmail.com>
2024-11-30 13:55:26 +0100

    Add search and apply buttons to header

--- src/Components/AppHeader/AppHeader.tsx
+++ src/Components/AppHeader/AppHeader.tsx
@@ -1,16 +1,28 @@
 import { Link } from "react-router";
 import "./AppHeader.scss";
+import Icon from "../Icon/Icon";
 
 export default function AppHeader() {
   return (
     <div id="app-header">
       <Link to="/">
         <img src="./logo.png" alt="Logo" />
+        ETS Zemun
       </Link>
 
-      <Link to="/news">News</Link>
-      <Link to="/info">Info</Link>
-      <Link to="/profiles">Profiles</Link>
+      <div className="nav-bar">
+        <Link to="/news">News</Link>
+        <Link to="/info">Info</Link>
+        <Link to="/profiles">Profiles</Link>
+      </div>
+
+      <button className="apply-button">
+        <Link to="/apply">Apply now</Link>
+      </button>
+
+      <button className="search-button">
+        <Icon name="magnifying-glass" />
+      </button>
     </div>
   );
 }
