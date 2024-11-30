172f70b62aafe0c5980e267873f1ee95e139f847
Andrej235 <andrej.nenadic06@gmail.com>
2024-11-30 14:52:29 +0100

    Add AppHeader css

--- src/Components/AppHeader/AppHeader.tsx
+++ src/Components/AppHeader/AppHeader.tsx
@@ -5,15 +5,17 @@ import Icon from "../Icon/Icon";
 export default function AppHeader() {
   return (
     <div id="app-header">
-      <Link to="/">
+      <Link to="/" className="logo">
         <img src="./logo.png" alt="Logo" />
         ETS Zemun
       </Link>
 
       <div className="nav-bar">
-        <Link to="/news">News</Link>
-        <Link to="/info">Info</Link>
-        <Link to="/profiles">Profiles</Link>
+        <Link to="/">O nama</Link>
+        <Link to="/">Obrazovni profili</Link>
+        <Link to="/">Radnici</Link>
+        <Link to="/">Prijava</Link>
+        <Link to="/">Novosti</Link>
       </div>
 
       <Link className="apply-link" to="/apply">
