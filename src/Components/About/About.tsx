0009b44cd8d61ade792328ba26fa602cf68f3cc2
Andrej235 <andrej.nenadic06@gmail.com>
2024-12-01 15:52:41 +0100

    Add a placeholder overview section

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -27,6 +27,35 @@ export default function About() {
           </div>
         </div>
       </div>
+
+      <div className="overview">
+        <h1>Lorem ipsum dolor sit amet.</h1>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+          possimus tempore accusantium facere earum ab eligendi repellendus
+          perspiciatis vero, iste obcaecati sed. Inventore atque beatae ipsum.
+          Distinctio doloribus, odio a ipsa corrupti neque officiis nulla
+          dignissimos! Quia non placeat animi eum labore rem! Ullam ab,
+          distinctio suscipit sequi, ut ipsum et incidunt blanditiis laboriosam
+          veniam vitae aliquid! Sequi ea obcaecati et asperiores tempora alias
+          rem rerum cumque laborum voluptas error nam, fuga temporibus a quod
+          atque animi incidunt fugiat magnam odio maxime blanditiis minus vero
+          nisi! Eligendi magni, odit quisquam neque a error, facilis qui minima
+          debitis est officiis quos ullam labore perferendis repellat sunt,
+          pariatur modi ipsum consectetur possimus rerum inventore saepe. Cum
+          consequuntur suscipit, iusto deserunt in eveniet dolore blanditiis
+          facilis sequi, soluta magnam numquam voluptatum corrupti, facere
+          possimus ratione autem. Culpa mollitia voluptate autem deleniti quos
+          quasi reiciendis veniam nisi numquam at alias, illum corrupti iste
+          similique debitis velit harum aspernatur nobis dolorem libero quia,
+          cupiditate iure fugiat necessitatibus. Et culpa ab sint eius magnam.
+          Nesciunt adipisci, eveniet qui, laborum eum incidunt distinctio
+          repellat officiis delectus voluptate omnis velit laboriosam
+          repudiandae totam et, deleniti dolor voluptatum culpa ab minus? Itaque
+          exercitationem tempore, odit eaque excepturi aliquid blanditiis.
+        </p>
+      </div>
     </div>
   );
 }
