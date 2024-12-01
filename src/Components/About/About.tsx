c0b7d90735173a1cf883cd2f8eaa0deeaf9c11b0
Andrej235 <andrej.nenadic06@gmail.com>
2024-12-01 20:48:40 +0100

    Add a footer to About page

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -192,6 +192,34 @@ export default function About() {
           image="/mock-news-preview/3.png"
         />
       </div>
+
+      <div className="footer">
+        <div className="social-media-icons-container">
+          <a href="https://www.linkedin.com">
+            <Icon name="linkedin" />
+          </a>
+
+          <a href="https://www.youtube.com">
+            <Icon name="youtube" />
+          </a>
+
+          <a href="https://www.x.com">
+            <Icon name="twitter" />
+          </a>
+
+          <a href="https://www.facebook.com">
+            <Icon name="facebook" />
+          </a>
+
+          <a href="https://www.instagram.com">
+            <Icon name="instagram" />
+          </a>
+        </div>
+
+        <h1 className="footer-title">
+          &copy; 2024 Elektrotehnička škola "Zemun"
+        </h1>
+      </div>
     </div>
   );
 }
