eb53d6413cc133d6c7832cf0795a98719fbea8b2
Aleksa Andjelic <23151811a@gmail.com>
2024-12-03 23:19:32 +0100

    Add div for every tab in navbar and style it

--- src/Components/HamburgerNavigation/HamburgerNavigation.tsx
+++ src/Components/HamburgerNavigation/HamburgerNavigation.tsx
@@ -1,4 +1,6 @@
+import { createPortal } from "react-dom";
 import "./HamburgerNavigation.scss";
+import { Link } from "react-router";
 
 type HamburgerNavigationProps = {
   isMenuActive: boolean;
@@ -7,11 +9,20 @@ type HamburgerNavigationProps = {
 export default function HamburgerNavigation({
   isMenuActive,
 }: HamburgerNavigationProps) {
-  return (
+  return createPortal(
     <div
       className={`hamburger-navigation${
         !isMenuActive ? " navigation-not-active" : ""
       }`}
-    ></div>
+    >
+      <div className="nav-bar">
+        <div className="nav-bar-tab"><Link to="/">O nama</Link></div>
+        <div className="nav-bar-tab"><Link to="/">Obrazovni profili</Link></div>
+        <div className="nav-bar-tab"><Link to="/">Ucenici</Link></div>
+        <div className="nav-bar-tab"><Link to="/">Novosti</Link></div>
+        <div className="nav-bar-tab"><Link to="/">Dokumenta</Link></div>
+      </div>
+    </div>,
+    document.body
   );
 }
