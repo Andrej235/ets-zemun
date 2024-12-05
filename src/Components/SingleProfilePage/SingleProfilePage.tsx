3c091772fad6c4dd6434f11b92addea0947d9e18
Andrej235 <andrej.nenadic06@gmail.com>
2024-12-05 19:50:31 +0100

    Roughly display all profile data in SingleProfile page

--- src/Components/SingleProfilePage/SingleProfilePage.tsx
+++ src/Components/SingleProfilePage/SingleProfilePage.tsx
@@ -5,5 +5,44 @@ export default function SingleProfilePage() {
   const loaderData = useLoaderData() as ProfileSchema;
   console.log(loaderData.classes);
 
-  return <div>SingleProfilePage</div>;
+  return (
+    <div>
+      <h1>{loaderData.name}</h1>
+      <p>{loaderData.description}</p>
+      <img src={loaderData.imagePath} alt={loaderData.name} />
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              <h1>Opste obrazovni predmeti</h1>
+            </td>
+          </tr>
+
+          {loaderData.classes.general.map((x) => (
+            <tr key={x.name}>
+              <td>{x.name}</td>
+              {x.perWeek.map((count, i) => (
+                <td key={i}>{count}</td>
+              ))}
+            </tr>
+          ))}
+
+          <tr>
+            <td>
+              <h1>Strucni predmeti</h1>
+            </td>
+          </tr>
+
+          {loaderData.classes.specific.map((x) => (
+            <tr key={x.name}>
+              <td>{x.name}</td>
+              {x.perWeek.map((count, i) => (
+                <td key={i}>{count}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
