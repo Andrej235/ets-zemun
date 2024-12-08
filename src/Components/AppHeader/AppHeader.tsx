fc788fd40498f5cb5c02df382d87a5864d79d928
Andrej235 <andrej.nenadic06@gmail.com>
2024-12-08 12:08:50 +0100

    Restructure HamburgerMenu and make it more accessible

--- src/Components/AppHeader/AppHeader.tsx
+++ src/Components/AppHeader/AppHeader.tsx
@@ -2,27 +2,36 @@ import "./AppHeader.scss";
 import { Link } from "react-router";
 import HamburgerMenu from "../HamburgerMenu/HamburgerMenu";
 import HeaderSearchBar from "../SearchBar/SearchBar";
+import { useState } from "react";
+import FocusTrap from "focus-trap-react";
 
 export default function AppHeader() {
+  const [isFocusTrapActive, setIsFocusTrapActive] = useState(false);
+
   return (
-    <div id="app-header">
-      <HamburgerMenu />
+    <FocusTrap active={isFocusTrapActive && false}>
+      <div id="app-header">
+        <HamburgerMenu
+          onOpen={() => setIsFocusTrapActive(true)}
+          onClose={() => setIsFocusTrapActive(false)}
+        />
 
-      <Link to="/" className="logo">
-        <img src="./logo.png" alt="Logo" />
-      </Link>
+        <Link to="/" className="logo">
+          <img src="./logo.png" alt="Logo" />
+        </Link>
 
-      <div className="app-header-navigation">
-        <div className="nav-bar">
-          <Link to="/">O nama</Link>
-          <Link to="/profili">Obrazovni profili</Link>
-          <Link to="/ucenici">Ucenici</Link>
-          <Link to="/novosti">Novosti</Link>
-          <Link to="/dokumenta">Dokumenta</Link>
+        <div className="app-header-navigation">
+          <div className="nav-bar">
+            <Link to="/">O nama</Link>
+            <Link to="/profili">Obrazovni profili</Link>
+            <Link to="/ucenici">Ucenici</Link>
+            <Link to="/novosti">Novosti</Link>
+            <Link to="/dokumenta">Dokumenta</Link>
+          </div>
+
+          <HeaderSearchBar />
         </div>
-
-        <HeaderSearchBar />
       </div>
-    </div>
+    </FocusTrap>
   );
 }
--- src/Components/HamburgerMenu/HamburgerMenu.tsx
+++ src/Components/HamburgerMenu/HamburgerMenu.tsx
@@ -2,26 +2,39 @@ import "./HamburgerMenu.scss";
 import { useState } from "react";
 import HamburgerNavigation from "../HamburgerNavigation/HamburgerNavigation";
 
-function HamburgerMenu() {
+type HamburgerMenuProps = {
+  onOpen?: () => void;
+  onClose?: () => void;
+};
 
-  const [isChecked, setisChecked] = useState(false)
+function HamburgerMenu({ onOpen, onClose }: HamburgerMenuProps) {
+  const [isChecked, setisChecked] = useState(false);
 
   return (
     <>
-      <div className="hamburger-menu-wrapper">
-        <label className="hamburger">
-          <input type="checkbox" onClick={() => setisChecked((prevState) => !prevState)}/>
+      <div className="hamburger-menu-container">
+        <button
+          className={"hamburger-menu" + (isChecked ? " open" : "")}
+          onClick={(e) => {
+            isChecked ? onClose?.() : onOpen?.();
+            setisChecked((prevState) => !prevState);
+
+            e.stopPropagation();
+            e.preventDefault();
+          }}
+        >
           <svg viewBox="0 0 32 32">
             <path
               className="line line-top-bottom"
               d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22"
             />
+
             <path className="line" d="M7 16 27 16" />
           </svg>
-        </label>
+        </button>
       </div>
 
-      <HamburgerNavigation isMenuActive={isChecked}/>
+      <HamburgerNavigation isMenuActive={isChecked} />
     </>
   );
 }
