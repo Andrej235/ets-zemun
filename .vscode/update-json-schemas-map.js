0ef8351bbecf1043b9144cb4d8169ed465c184f0
Andrej235 <andrej.nenadic06@gmail.com>
2024-12-20 18:53:29 +0100

    Create a data-to-schema-map for mapping json files to their respective schema files and add an on save command which will copy all data from this file into .vscode/settings.json

---
+++ .vscode/update-json-schemas-map.js
@@ -0,0 +1,19 @@
+import fs from "fs/promises";
+import { argv } from "process";
+
+console.log(argv[2]);
+
+const currentMap = await fs
+  .readFile(`${argv[2]}/src/assets/json-data/data-to-schema-map.json`, "utf-8")
+  .then(JSON.parse);
+
+const settingsJson = await fs
+  .readFile(`${argv[2]}/.vscode/settings.json`, "utf-8")
+  .then(JSON.parse);
+
+settingsJson["json.schemas"] = currentMap;
+
+await fs.writeFile(
+  `${argv[2]}/.vscode/settings.json`,
+  JSON.stringify(settingsJson, null, 2)
+);
