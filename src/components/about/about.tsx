0590c4849c45a90e1c059be1027ca32654137aea
Aleksa Andjelic <23151811a@gmail.com>
2025-01-05 16:57:54 +0100

    Add fixed height to InfoCard component

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -86,7 +86,7 @@ export default function About() {
           </motion.p>
         </div>
 
-        <AliceCarousel itemsPerSlide={5}>
+        <AliceCarousel itemsPerSlide={3}>
           <InfoCard
             icon="history"
             title="Istorija škole"
