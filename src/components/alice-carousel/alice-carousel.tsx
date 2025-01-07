45d979cb73a477dea2cbd54dee01e274f9bb4f10
Aleksa Andjelic <23151811a@gmail.com>
2025-01-07 20:33:19 +0100

    Create visibleItemsArray that will change based on what button was clicked

--- src/components/alice-carousel/alice-carousel.tsx
+++ src/components/alice-carousel/alice-carousel.tsx
@@ -8,41 +8,33 @@ type AliceCarouselProps = {
 
 export default function AliceCarousel({
   children,
-  itemsPerSlide,
 }: AliceCarouselProps) {
   const childrenArray = React.Children.toArray(children);
 
-  const [startIndex, setStartIndex] = useState(0);
-
-  const totalItems = childrenArray.length;
+  const [visibleItemsArray, setVisibleItemsArray] = useState(() => {
+    if (childrenArray.length > 1) {
+      const lastItem = childrenArray[childrenArray.length - 1];
+      const firstItem = childrenArray[0]; 
+      return [lastItem, ...childrenArray, firstItem];
+    } else {
+      return childrenArray;
+    }
+  });
 
   const goToNext = () => {
-    setStartIndex((prevIndex) => (prevIndex + 1) % totalItems);
   };
 
   const goToPrev = () => {
-    setStartIndex(
-      (prevIndex) => (prevIndex - 1 + totalItems) % totalItems
-    );
   };
-
-  const visibleItems = Array.from({ length: itemsPerSlide }, (_, i) => {
-    const index = (startIndex + i) % totalItems;
-    return childrenArray[index];
-  });
 
   return (
     <div className="alice-carousel-container">
       <div className="carousel-content">
-        <div className="carousel-wrapper">
-          <div className="carousel-section">
-            {visibleItems.map((child, childIndex) => (
+            {visibleItemsArray.map((child, childIndex) => (
               <div className="carousel-item" key={childIndex}>
                 {child}
               </div>
             ))}
-          </div>
-        </div>
       </div>
 
       <button className="arrow-left" onClick={goToPrev}>
