9c185d9af329ca94ccd3ed54f736b12139895db4
Aleksa Andjelic <23151811a@gmail.com>
2025-01-10 19:36:10 +0100

    Implement AliceCarousel component

--- src/components/alice-carousel/alice-carousel.tsx
+++ src/components/alice-carousel/alice-carousel.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
 import "./alice-carousel.scss";
 
 type AliceCarouselProps = {
@@ -6,123 +8,40 @@ type AliceCarouselProps = {
   itemsPerSlide: number;
 };
 
-export default function AliceCarousel({
-  children,
-  itemsPerSlide,
-}: AliceCarouselProps) {
+export default function CustomAliceCarousel({ children }: AliceCarouselProps) {
   const childrenArray = React.Children.toArray(children);
 
-  const [visibleItemsArray, setVisibleItemsArray] = useState(() => {
-    if (childrenArray.length > 1) {
-      const lastItem = childrenArray[childrenArray.length - 1];
-      const firstItem = childrenArray[0];
-      return [lastItem, ...childrenArray, firstItem];
-    } else {
-      return childrenArray;
-    }
-  });
-  const [nextIndex, setNextIndex] = useState(1);
-  const [prevIndex, setPrevIndex] = useState(childrenArray.length - 2);
-  const [changePosition, setChangePosition] = useState<" prev" | " next" | "">(
-    ""
-  );
-  const [isButtonPressed, setIsButtonPressed] = useState<boolean>(false);
+  const items = childrenArray.map((child, index) => (
+    <div key={index} className="carousel-item">
+      {child}
+    </div>
+  ));
 
-  const goToNext = () => {
-    if (!isButtonPressed) {
-      setChangePosition(" next");
-      setVisibleItemsArray((prevArray) => {
-        const updatedArray = [...prevArray.slice(1), childrenArray[nextIndex]];
-        nextIndex !== childrenArray.length - 1
-          ? setNextIndex(nextIndex + 1)
-          : setNextIndex(0);
-        return updatedArray;
-      });
-      setIsButtonPressed(true);
-
-      setTimeout(() => {
-        setChangePosition("");
-      }, 1);
-      setTimeout(() => setIsButtonPressed(false), 400);
-    }
-  };
-
-  const goToPrev = () => {
-    if (!isButtonPressed) {
-      setVisibleItemsArray((prevArray) => {
-        const updatedArray = [
-          childrenArray[prevIndex],
-          ...prevArray.slice(0, -1),
-        ];
-        prevIndex === 0
-          ? setPrevIndex(childrenArray.length - 1)
-          : setPrevIndex(prevIndex - 1);
-        return updatedArray;
-      });
-      setIsButtonPressed(true);
-
-      setChangePosition(" prev");
-      setTimeout(() => {
-        setChangePosition("");
-      }, 1);
-      setTimeout(() => setIsButtonPressed(false), 400);
-    }
-  };
-
-  const getClassNameFromActiveIndex = (
-    index: number,
-    arrayLength: number,
-    itemsPerSlide: number
-  ) => {
-    const middleIndex = Math.floor(arrayLength / 2);
-    const startIndex = Math.max(0, middleIndex - Math.floor(itemsPerSlide / 2));
-    const endIndex = Math.min(
-      arrayLength - 1,
-      middleIndex + Math.floor(itemsPerSlide / 2)
-    );
-
-    const distanceFromMiddle = Math.abs(middleIndex - index);
-
-    const maxScale = 1.2;
-    const scale = Math.max(1, maxScale - distanceFromMiddle * 0.05);
-
-    const scaleClass = `scale_${scale.toFixed(2).replace(".", "_")}`;
-
-    if (index < startIndex || index > endIndex) {
-      return " outside";
-    } else if (index === middleIndex) {
-      return " middle";
-    } else if (index < middleIndex) {
-      return ` grow-left ${scaleClass}`;
-    } else {
-      return ` grow-right ${scaleClass}`;
-    }
+  const responsive = {
+    0: {
+      items: 1,
+    },
+    425: {
+      items: 2,
+    },
+    768: {
+      items: 3,
+    },
+    1440: {
+      items: 5,
+    },
   };
 
   return (
     <div className="alice-carousel-container">
-      <div className={`carousel-content${changePosition}`}>
-        {visibleItemsArray.map((child, childIndex) => (
-          <div
-            className={`carousel-item${getClassNameFromActiveIndex(
-              childIndex,
-              visibleItemsArray.length,
-              itemsPerSlide
-            )}`}
-            key={childIndex}
-          >
-            {child}
-          </div>
-        ))}
+      <div className="alice-carousel-wrapper">
+        <AliceCarousel
+          mouseTracking
+          infinite
+          responsive={responsive}
+          items={items}
+        />
       </div>
-
-      <button className="arrow-left" onClick={goToPrev}>
-        &#60;
-      </button>
-
-      <button className="arrow-right" onClick={goToNext}>
-        &#62;
-      </button>
     </div>
   );
 }
