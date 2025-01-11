3a772fa864dd2a154df8408c53d726b2d3215b58
Aleksa Andjelic <23151811a@gmail.com>
2025-01-11 13:37:35 +0100

    Remove OverlayScroll component from about

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -1,40 +1,15 @@
-import {
-  OverlayScrollbarsComponent,
-  OverlayScrollbarsComponentRef,
-} from "overlayscrollbars-react";
 import CustomSwiper from "../custom-swiper/custom-swiper";
 import Icon from "@components/icon/icon";
 import InfoCard from "@components/info-card/info-card";
 import "./about.scss";
 import ProfileOverview from "@components/profile-preview/profile-preview";
-import { animate, motion, scroll } from "motion/react";
-import { useEffect, useRef } from "react";
+import { motion } from "motion/react";
 import NewsAndEventsPreviewContainer from "@components/news-and-events-preview-container/news-and-events-preview-container";
 import data from "@data/profiles.json";
 import ProfileOverviewSchema from "src/assets/json-data/ts-schemas/profile-overview.schema";
 import scrollAnimationFlyInBottom from "../../motion-animation-presets/scroll-animation-fly-in-bottom";
 
 export default function About() {
-  const infoCardsContainerRef = useRef<OverlayScrollbarsComponentRef>(null);
-
-  useEffect(() => {
-    if (!infoCardsContainerRef.current) return;
-    const infoCardsContainer = infoCardsContainerRef.current
-      .getElement()!
-      .querySelector("[data-overlayscrollbars-contents]")! as HTMLElement;
-
-    for (let i = 0; i < infoCardsContainer.children.length; i++) {
-      const card = infoCardsContainer.children[i];
-
-      scroll(animate(card, { opacity: [0, 1, 1, 0] }, { ease: "linear" }), {
-        axis: "x",
-        target: card,
-        container: infoCardsContainer,
-        offset: ["start end", "end end", "start start", "end start"],
-      });
-    }
-  }, []);
-
   return (
     <div id="about-page">
       <section>
@@ -150,84 +125,6 @@ export default function About() {
             text="Škola se pridržava svih zakonskih propisa i pravnih akata, garantujući pravo svakog učenika na sigurno i kvalitetno obrazovanje."
           />
         </CustomSwiper>
-
-        <OverlayScrollbarsComponent
-          className="info-cards-container"
-          options={{
-            scrollbars: {
-              autoHide: "leave",
-              autoHideDelay: 150,
-            },
-            overflow: {
-              x: "scroll",
-              y: "hidden",
-            },
-          }}
-          ref={infoCardsContainerRef}
-        >
-          <InfoCard
-            icon="history"
-            title="Istorija škole"
-            link="/istorija"
-            text="Sa tradicijom dugom više od 130 godina, škola nudi savremene programe u skladu sa potrebama 21. veka."
-          />
-
-          <InfoCard
-            icon="graduation-cap"
-            title="Programi obrazovanja"
-            link="/programi"
-            text="Nudimo širok spektar tehničkih i stručnih smerova koji pripremaju učenike za uspeh u industriji."
-          />
-
-          <InfoCard
-            icon="building"
-            title="Infrastruktura i oprema"
-            link="/infrastruktura"
-            text="Moderne učionice i specijalizovane laboratorije omogućavaju učenicima kvalitetno obrazovanje i praktičan rad."
-          />
-
-          <InfoCard
-            icon="chalkboard-teacher"
-            title="Nastavnici"
-            link="/nastavnici"
-            text="Naši nastavnici su iskusni profesionalci koji inspirišu i podržavaju razvoj učenika kroz inovativne metode nastave."
-          />
-
-          <InfoCard
-            icon="trophy"
-            title="Takmičenja i nagrade"
-            link="/takmicenja"
-            text="Škola je ponosna na brojne nagrade i priznanja koja su naši učenici osvojili na domaćim i međunarodnim takmičenjima."
-          />
-
-          <InfoCard
-            icon="lightbulb"
-            title="Mentorski programi"
-            link="/mentori"
-            text="Naši mentori pomažu učenicima da razvijaju veštine i planiraju svoj profesionalni put uz podršku kroz različite aktivnosti."
-          />
-
-          <InfoCard
-            icon="pencil-alt"
-            title="Upis i prijem"
-            link="/upis"
-            text="Jednostavan proces upisa i pomoć u odabiru smera omogućavaju lakše snalaženje budućim studentima."
-          />
-
-          <InfoCard
-            icon="headset"
-            title="Podrška učenicima"
-            link="/podrska"
-            text="Naša psihološko-pedagoška služba pruža podršku učenicima i roditeljima u svim izazovima, uključujući prevenciju nasilja i krizne intervencije."
-          />
-
-          <InfoCard
-            icon="gavel"
-            title="Pravni akte"
-            link="/pravni-akte"
-            text="Škola se pridržava svih zakonskih propisa i pravnih akata, garantujući pravo svakog učenika na sigurno i kvalitetno obrazovanje."
-          />
-        </OverlayScrollbarsComponent>
       </section>
 
       <div className="profiles-overview-container">
