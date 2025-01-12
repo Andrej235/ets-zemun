71273c3d3eba4a88d30e9efc943ce46e9824f22b
Aleksa Andjelic <23151811a@gmail.com>
2025-01-12 14:42:16 +0100

    Remove overview section in About component

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -28,40 +28,7 @@ export default function About() {
         </div>
       </section>
 
-      <section>
-        <div className="overview">
-          <motion.h1 {...scrollAnimationFlyInBottom}>
-            Lorem ipsum dolor sit amet.
-          </motion.h1>
-
-          <motion.p {...scrollAnimationFlyInBottom}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
-            possimus tempore accusantium facere earum ab eligendi repellendus
-            perspiciatis vero, iste obcaecati sed. Inventore atque beatae ipsum.
-            Distinctio doloribus, odio a ipsa corrupti neque officiis nulla
-            dignissimos! Quia non placeat animi eum labore rem! Ullam ab,
-            distinctio suscipit sequi, ut ipsum et incidunt blanditiis
-            laboriosam veniam vitae aliquid! Sequi ea obcaecati et asperiores
-            tempora alias rem rerum cumque laborum voluptas error nam, fuga
-            temporibus a quod atque animi incidunt fugiat magnam odio maxime
-            blanditiis minus vero nisi! Eligendi magni, odit quisquam neque a
-            error, facilis qui minima debitis est officiis quos ullam labore
-            perferendis repellat sunt, pariatur modi ipsum consectetur possimus
-            rerum inventore saepe. Cum consequuntur suscipit, iusto deserunt in
-            eveniet dolore blanditiis facilis sequi, soluta magnam numquam
-            voluptatum corrupti, facere possimus ratione autem. Culpa mollitia
-            voluptate autem deleniti quos quasi reiciendis veniam nisi numquam
-            at alias, illum corrupti iste similique debitis velit harum
-            aspernatur nobis dolorem libero quia, cupiditate iure fugiat
-            necessitatibus. Et culpa ab sint eius magnam. Nesciunt adipisci,
-            eveniet qui, laborum eum incidunt distinctio repellat officiis
-            delectus voluptate omnis velit laboriosam repudiandae totam et,
-            deleniti dolor voluptatum culpa ab minus? Itaque exercitationem
-            tempore, odit eaque excepturi aliquid blanditiis.
-          </motion.p>
-        </div>
-
-        <CustomSwiper>
+      <CustomSwiper>
           <InfoCard
             icon="history"
             title="Istorija škole"
@@ -125,7 +92,6 @@ export default function About() {
             text="Škola se pridržava svih zakonskih propisa i pravnih akata, garantujući pravo svakog učenika na sigurno i kvalitetno obrazovanje."
           />
         </CustomSwiper>
-      </section>
 
       <div className="profiles-overview-container">
         {data.profiles.map((profile, i) => (
@@ -171,3 +137,4 @@ export default function About() {
     </div>
   );
 }
+
