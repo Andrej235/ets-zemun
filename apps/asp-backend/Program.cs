f5a783280d01314f8d5df6519a6ba1763ec4b556
Andrej235 <andrej.nenadic06@gmail.com>
2025-01-21 22:41:35 +0100

    Setup a CORS policy which just lets everything through

--- apps/asp-backend/Program.cs
+++ apps/asp-backend/Program.cs
@@ -1,10 +1,16 @@
 using EtsZemun.Data;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
+
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+});
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -21,11 +27,12 @@ builder
     .AddGoogle(googleOptions =>
     {
         googleOptions.ClientId =
-            builder.Configuration["Authentication:Google:ClientId"]
+            builder.Configuration["Authentication__Google__ClientId"]
             ?? throw new Exception("Google ClientId is missing");
         googleOptions.ClientSecret =
-            builder.Configuration["Authentication:Google:ClientSecret"]
+            builder.Configuration["Authentication__Google__ClientSecret"]
             ?? throw new Exception("Google ClientSecret is missing");
+        googleOptions.CallbackPath = "/signin-google";
     });
 
 builder.Services.AddDbContext<DataContext>(options =>
@@ -33,7 +40,21 @@ builder.Services.AddDbContext<DataContext>(options =>
     options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(
+        "WebsitePolicy",
+        builder =>
+        {
+            builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+        }
+    );
+});
+
 var app = builder.Build();
+app.UseForwardedHeaders();
+app.UseCors("WebsitePolicy");
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
