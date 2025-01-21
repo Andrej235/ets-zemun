a8e2789adf2f382e8462190df47b5b71c1fe0d87
Aleksa Andjelic <23151811a@gmail.com>
2025-01-21 19:45:38 +0100

    Create hover effect for NewsPreview component

--- src/components/news-preview/news-preview.tsx
+++ src/components/news-preview/news-preview.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import "./news-preview.scss";
 
 type NewsPreviewProps = {
@@ -13,17 +14,34 @@ export default function NewsPreview({
   description,
   image,
 }: NewsPreviewProps) {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (e: React.MouseEvent) => {
+    const { left, top } = e.currentTarget.getBoundingClientRect();
+    const x = e.clientX - left;
+    const y = e.clientY - top;
+
+    setMousePosition({ x, y });
+  };
+
   return (
-    <div className="news-article-preview">
+    <div
+      className="news-article-preview"
+      onMouseMove={handleMouseMove}
+      style={{
+        "--mouse-x": `${mousePosition.x}px`,
+        "--mouse-y": `${mousePosition.y}px`,
+      } as React.CSSProperties}
+    >
       <div className="image-container">
         <img src={image} alt={title} />
       </div>
       <div className="info">
-        <h1 className="title">{title}</h1><br />
+        <h1 className="title">{title}</h1>
+        <br />
         <p className="description">{description}</p>
         <p className="date">{date.toLocaleDateString()}</p>
       </div>
     </div>
   );
 }
-
