800128729a350e22f2919f81cb7735337f26d1df
Andrej235 <andrej.nenadic06@gmail.com>
2025-01-29 13:05:31 +0100

    Implement footer

--- apps/website/src/components/app-footer/app-footer.tsx
+++ apps/website/src/components/app-footer/app-footer.tsx
@@ -4,6 +4,67 @@ import "./app-footer.scss";
 export default function AppFooter() {
   return (
     <footer id="app-footer">
+      <div className="info">
+        <div className="about">
+          <h1>O nama</h1>
+
+          <p>
+            Sa ciljom da obezbedimo kvalitetno obrazovanje i podstaknemo
+            samostalno učenje i razvoj učenika težimo tome da oni izrastu u
+            odgovorne i preduzimljive ličnosti u sigurnom i podsticajnom
+            okruženju
+          </p>
+        </div>
+
+        <div className="contact">
+          <h1>Kontakt</h1>
+
+          <div className="column">
+            <p>
+              <Icon name="location-dot" />
+              <span>Beograd – Zemun, Nade Dimić 4</span>
+            </p>
+
+            <p>
+              <Icon name="phone" />
+              <span>+381 11 316 1849</span>
+            </p>
+
+            <p>
+              <Icon name="phone" />
+              <span>+381 11 261 8155</span>
+            </p>
+
+            <p>
+              <Icon name="envelope" />
+              <span>{"skola@ets-zemun.edu.rs"}</span>
+            </p>
+          </div>
+
+          <div className="column">
+            <p>
+              <Icon name="user-tie" />
+              <span>{"direktor@ets-zemun.edu.rs"}</span>
+            </p>
+
+            <p>
+              <Icon name="user-edit" />
+              <span>{"sekretar@ets-zemun.edu.rs"}</span>
+            </p>
+
+            <p>
+              <Icon name="calculator" />
+              <span>{"rac@ets-zemun.edu.rs"}</span>
+            </p>
+
+            <p>
+              <Icon name="users" />
+              <span>{"ppsluzba@ets-zemun.edu.rs"}</span>
+            </p>
+          </div>
+        </div>
+      </div>
+
       <div className="social-media-icons-container">
         <a href="https://www.linkedin.com">
           <Icon name="linkedin" />
