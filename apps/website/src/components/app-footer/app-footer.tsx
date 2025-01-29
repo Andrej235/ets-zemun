5157a1a4a2ca8aaad674fb2c74ed3b19d9ec9965
Andrej235 <andrej.nenadic06@gmail.com>
2025-01-29 13:15:48 +0100

    Change footer's layout

--- apps/website/src/components/app-footer/app-footer.tsx
+++ apps/website/src/components/app-footer/app-footer.tsx
@@ -65,31 +65,26 @@ export default function AppFooter() {
         </div>
       </div>
 
-      <div className="social-media-icons-container">
-        <a href="https://www.linkedin.com">
-          <Icon name="linkedin" />
-        </a>
+      <div className="copyright">
+        <p className="name">
+          &copy; {" " + new Date().getFullYear() + " "} Elektrotehnička škola
+          "Zemun"
+        </p>
 
-        <a href="https://www.youtube.com">
-          <Icon name="youtube" />
-        </a>
+        <div className="social-media-links">
+          <a href={"https://www.linkedin.com"}>
+            <Icon name="linkedin" />
+          </a>
 
-        <a href="https://www.x.com">
-          <Icon name="twitter" />
-        </a>
+          <a href={"https://www.x.com"}>
+            <Icon name="x-twitter" />
+          </a>
 
-        <a href="https://www.facebook.com">
-          <Icon name="facebook" />
-        </a>
-
-        <a href="https://www.instagram.com">
-          <Icon name="instagram" />
-        </a>
+          <a href={"https://www.instagram.com"}>
+            <Icon name="instagram" />
+          </a>
+        </div>
       </div>
-
-      <h1 className="footer-title">
-        &copy; 2024 Elektrotehnička škola "Zemun"
-      </h1>
     </footer>
   );
 }
