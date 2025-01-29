97fd70859076c77b7c9462683c857edcf9e413a9
Andrej235 <andrej.nenadic06@gmail.com>
2025-01-29 11:27:52 +0100

    Animate four leaf clover's leaf count from 0 to whatever it is supposed to be when entering the viewport

--- apps/website/src/four-leaf-clover/four-leaf-clover.tsx
+++ apps/website/src/four-leaf-clover/four-leaf-clover.tsx
@@ -1,3 +1,5 @@
+import { animate, inView } from "motion/react";
+import { useEffect, useRef, useState } from "react";
 import "./four-leaf-clover.scss";
 
 export default function FourLeafClover() {
@@ -17,10 +19,36 @@ type LeafProps = {
 };
 
 function FourLeafCloverLeaf({ title, count }: LeafProps) {
+  const [currentCount, setCurrentCount] = useState(0);
+  const countRef = useRef<HTMLHeadingElement>(null);
+
+  useEffect(() => {
+    if (!countRef.current) return;
+
+    const cleanup = inView(countRef.current, () => {
+      animate(0, count, {
+        duration: 1.5,
+        ease: "easeOut",
+        onUpdate: (latest) => {
+          setCurrentCount(Math.floor(latest));
+        },
+      });
+
+      return () => {
+        setCurrentCount(0); //? Reset the count when the element is out of view, this ensures that the count will start from 0 when the element is in view again
+      };
+    });
+
+    return cleanup;
+  }, [countRef, count]);
+
   return (
     <div className="four-leaf-clover-leaf">
-      <div className="count">{count}+</div>
-      <div className="title">{title}</div>
+      <h1 className="count" ref={countRef}>
+        {currentCount}+
+      </h1>
+
+      <h2 className="title">{title}</h2>
     </div>
   );
 }
