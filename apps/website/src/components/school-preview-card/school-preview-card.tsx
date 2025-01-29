43e17aa711a9673e98d57a8232bed0f1caf14e68
Andrej235 <andrej.nenadic06@gmail.com>
2025-01-29 21:01:11 +0100

    Animate school preview card's count

--- apps/website/src/components/school-preview-card/school-preview-card.tsx
+++ apps/website/src/components/school-preview-card/school-preview-card.tsx
@@ -2,6 +2,9 @@ import PreviewCard, {
   PreviewCardLayout,
 } from "@components/preview-card/preview-card";
 import "./school-preview-card.scss";
+import { useEffect, useRef, useState } from "react";
+import { animate } from "motion";
+import { inView } from "motion/react";
 
 type SchoolPreviewCardProps = {
   readonly layout: PreviewCardLayout;
@@ -18,11 +21,30 @@ export default function SchoolPreviewCard({
   layout,
   title,
 }: SchoolPreviewCardProps) {
+  const [currentCount, setCurrentCount] = useState(0);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!containerRef.current) return;
+
+    const cleanup = inView(containerRef.current, () => {
+      animate(0, count, {
+        duration: 1.5,
+        ease: "easeOut",
+        onUpdate: (latest) => {
+          setCurrentCount(Math.floor(latest));
+        },
+      });
+    });
+
+    return cleanup;
+  }, [containerRef, count]);
+
   return (
     <PreviewCard imagePath={image} layout={layout} imageAlt={title}>
-      <div className={"school-preview-card " + layout}>
+      <div className={"school-preview-card " + layout} ref={containerRef}>
         <h1 className="title">
-          <span>{count}+</span> {" " + title}
+          <span>{currentCount}+</span> {" " + title}
         </h1>
 
         <p className="description">{description}</p>
