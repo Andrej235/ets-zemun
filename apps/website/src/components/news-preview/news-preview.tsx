2e8540cef0c9434df42089e892dbc788d1d4ad33
Andrej235 <andrej.nenadic06@gmail.com>
2025-01-29 11:59:11 +0100

    Improve news preview's hover effect by making it much more efficient and removing it from mobile

--- apps/website/src/components/news-preview/news-preview.tsx
+++ apps/website/src/components/news-preview/news-preview.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import "./news-preview.scss";
+import { PointerEvent, useEffect, useRef } from "react";
 import { Link } from "react-router";
+import "./news-preview.scss";
 
 type NewsPreviewProps = {
   readonly date: Date;
@@ -15,27 +15,36 @@ export default function NewsPreview({
   description,
   image,
 }: NewsPreviewProps) {
-  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  const containerRef = useRef<HTMLAnchorElement>(null);
+  const containerPosition = useRef({ x: 0, y: 0 });
 
-  const handleMouseMove = (e: React.MouseEvent) => {
-    const { left, top } = e.currentTarget.getBoundingClientRect();
-    const x = e.clientX - left;
-    const y = e.clientY - top;
+  useEffect(() => {
+    if (!containerRef.current) return;
 
-    setMousePosition({ x, y });
+    const rect = containerRef.current.getBoundingClientRect();
+    containerPosition.current = {
+      x: rect.left,
+      y: rect.top,
+    };
+  }, [containerRef]);
+
+  const handleMouseMove = (e: PointerEvent) => {
+    if (e.pointerType !== "mouse") return;
+
+    const target = e.currentTarget as HTMLAnchorElement;
+    const x = e.clientX - containerPosition.current.x;
+    const y = e.clientY - containerPosition.current.y;
+
+    target.style.setProperty("--mouse-x", `${x}px`);
+    target.style.setProperty("--mouse-y", `${y}px`);
   };
 
   return (
     <Link
       to="/news/1"
       className="news-article-preview"
-      onMouseMove={handleMouseMove}
-      style={
-        {
-          "--mouse-x": `${mousePosition.x}px`,
-          "--mouse-y": `${mousePosition.y}px`,
-        } as React.CSSProperties
-      }
+      onPointerMove={handleMouseMove}
+      ref={containerRef}
     >
       <div className="image-container">
         <img src={image} alt={title} />
