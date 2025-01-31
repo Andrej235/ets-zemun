43fc6cba23575ee93324df469f656f16d51c41b8
Andrej235 <andrej.nenadic06@gmail.com>
2025-01-31 16:08:01 +0100

    Replace placeholders with actual data

--- apps/website/src/components/news-and-events-preview-container/news-and-events-preview-container.tsx
+++ apps/website/src/components/news-and-events-preview-container/news-and-events-preview-container.tsx
@@ -51,24 +51,24 @@ export default function NewsAndEventsPreviewContainer() {
         <section className="news-and-events-section">
           <div className="news-events-container">
             <NewsPreview
-              date={new Date()}
-              title="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae, sapiente?"
-              description="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit quasi cumque enim dolores molestiae dolorum dolore accusamus! In, praesentium quibusdam."
-              image="/mock-news-preview/1.jpg"
+              date={new Date("2024-10-28")}
+              title="Beograd ispod Beograda"
+              description="Odeljenje 2/4 sa razrednim starešinom Marinom Ristanović danas je istražilo tajne podzemnog Beograda. U pratnji stručnog vodiča, posetili su Karađorđevu kapiju, bunker iz 1948, Rimski bunar i barutanu sa rimskim sarkofazima. Obilazak je započet kod spomenika Francuskoj na Kalemegdanu, a završen u Donjem gradu uz priču o majoru Gavriloviću i Fransisu Makenziju. Nezaboravno putovanje kroz istoriju!"
+              image="/images/news/underground.jpg"
             />
 
             <NewsPreview
-              date={new Date("2024-05-15")}
-              title="Lorem ipsum, dolor sit amet consectetur adipisicing elit."
-              description="Lorem ipsum dolor sit amet consectetur, adipisicing elitadipisicing elit. Minus amet ea saepe corporis, quam labore officia obcaecati necessitatibus itaque quo optio? Adipisci asperiores pariatur rerum perspiciatis tempore odit rem, fugit dolorem quam doloremque voluptatum officiis magnam error dignissimos."
-              image="/mock-news-preview/2.jpg"
+              date={new Date("2024-10-15")}
+              title="Dan škole 2024"
+              description="Svečana sala škole pretvorila se 5. oktobra u pozornicu mladih umetnika. Učenici su kroz kombinovanu predstavu — od klasičnih pozorišnih scena do modernih koreografija i horskog pevanja — proslavili Dan škole. Nastup je uključivao i multimedijalne projekcije, oslikavajući istoriju škole i njene vrednosti. 'Ovo je dan kada škola sija kroz svoje učenike', istakla je direktorka."
+              image="/images/news/school-day2.jpg"
             />
 
             <NewsPreview
-              date={new Date("2024-01-23")}
-              title="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam voluptatibus minima!"
-              description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem quam doloremque voluptatum officiis magnam error dignissimos vitae excepturi temporibus maiores reiciendis veniam ipsam! Tempora nobis aliquid eligendi qui."
-              image="/mock-news-preview/3.jpg"
+              date={new Date("2024-02-24")}
+              title="Poezija uživo!"
+              description="Šesti ciklus 'Poezija uživo!' Radio Beograda 2 i Zadužbine Kolarca posvećen je Srđanu Gagiću, pesniku i profesoru. U subotu, 24. februara u 19:00, u Kolarčevoj zadužbini oživeće njegove stihove uz direktan prenos na radiju. Gagić, dobitnik Brankove nagrade za knjigu Deca u izlogu, deliće svoju poeziju sa publikom. Ulaznice su dostupne na blagajni Zadužbine."
+              image="/images/news/live-poetry.jpg"
             />
           </div>
 
