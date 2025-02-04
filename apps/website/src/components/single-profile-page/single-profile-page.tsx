60c4232f4ccb2a23575d3357dcac9f7861380f75
Andrej235 <andrej.nenadic06@gmail.com>
2025-02-04 14:30:48 +0100

    Add more text content to profile page

--- apps/website/src/components/single-profile-page/single-profile-page.tsx
+++ apps/website/src/components/single-profile-page/single-profile-page.tsx
@@ -1,9 +1,9 @@
-import useOutsideClick from "@hooks/use-outside-click";
-import "./single-profile-page.scss";
 import ProfileSchema from "@assets/json-data/ts-schemas/profile.schema";
+import useOutsideClick from "@hooks/use-outside-click";
 import { AnimatePresence, motion } from "motion/react";
 import { useMemo, useRef, useState } from "react";
 import { useLoaderData } from "react-router";
+import "./single-profile-page.scss";
 
 type ClassItem = {
   className: string;
@@ -68,76 +68,122 @@ export default function SingleProfilePage() {
             <motion.p layout>{selectedClass.count}x nedeljno</motion.p>
 
             <button onClick={() => console.log("hi")}>Hi</button>
-
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta
-              aut quia quasi obcaecati quam expedita libero vitae vero! Vel in
-              magni rem expedita minima exercitationem doloremque id magnam,
-              commodi quos veniam sint maxime earum? Soluta iure veritatis eius
-              illo doloremque magnam nesciunt est rerum laborum! Ea dolorem quam
-              rem labore quasi sit tempora quis possimus veritatis. Velit
-              nostrum consectetur sunt, autem id accusamus reiciendis ipsum
-              libero sapiente rerum ad nesciunt odit mollitia quibusdam
-              repellendus provident culpa praesentium voluptate voluptas?
-              Explicabo blanditiis illum quas, provident dolore molestias quod
-              similique sunt ex dolorem sed facere iste quo nobis optio tempora
-              vitae! Sint nesciunt aperiam culpa veritatis. Voluptatibus officia
-              iusto quis ducimus rem. Similique, beatae facilis? Ipsa voluptatem
-              nam iure repellat earum id molestiae, sunt nobis magnam delectus
-              dolore voluptatum corrupti voluptas officiis quae facere illum
-              esse beatae! Dolor repudiandae, magnam nostrum libero repellat
-              nisi, laudantium nulla dolores quod hic dolore aut, assumenda
-              quidem quam rerum sed totam nobis ea consequuntur provident vel!
-              Optio qui explicabo nihil velit eveniet alias modi, quaerat
-              adipisci ex illo. Quaerat dignissimos deserunt sequi et hic
-              possimus, velit adipisci eos dolore incidunt facere voluptatibus
-              placeat nobis fuga id sint magni minus distinctio consectetur
-              doloremque fugiat aliquid aliquam dolor? Dicta ex animi quibusdam
-              maiores nulla! Reprehenderit pariatur sunt velit. Rerum natus
-              molestiae nisi sequi ab quibusdam quaerat voluptatibus voluptas
-              voluptatum quos delectus sed tempora harum distinctio laboriosam
-              at optio esse illum, impedit cum eius quod fugit illo eveniet?
-              Accusamus sapiente, consectetur eius consequatur eum mollitia qui
-              reiciendis voluptatem dolore earum. Deserunt praesentium ea id
-              inventore eligendi voluptates veritatis, maxime dolorem nostrum
-              repellendus culpa distinctio voluptas omnis velit? Porro deserunt
-              sapiente minus tenetur, quaerat blanditiis hic alias impedit
-              molestiae eos minima quibusdam aut ratione repellat laudantium
-              quam? Culpa expedita incidunt ab tenetur, doloremque cupiditate
-              dolore libero autem perspiciatis pariatur sunt corporis placeat
-              impedit, assumenda eius facilis omnis officia unde tempora
-              voluptatem reprehenderit optio voluptas! Inventore reprehenderit,
-              qui vero repellendus illum, asperiores ratione quae excepturi
-              praesentium natus quod rem eveniet blanditiis eum culpa in
-              cupiditate impedit sapiente alias nobis, iste aperiam adipisci
-              corrupti. Illum, blanditiis? Asperiores recusandae aliquam rem
-              autem, possimus architecto illum nam. Excepturi quae corporis
-              mollitia expedita minima dicta maiores aliquid, impedit alias
-              perferendis iusto nulla dolor neque perspiciatis? Numquam odit
-              corporis libero dicta omnis quia obcaecati esse eos reprehenderit
-              doloribus blanditiis consequatur neque, nesciunt dignissimos
-              eligendi et beatae cumque autem porro debitis id impedit
-              aspernatur. Distinctio deserunt illo quos quaerat veniam ipsum
-              quibusdam consectetur odit dolores esse incidunt possimus ad error
-              debitis officiis magnam atque animi perferendis id, quisquam quae
-              quia eligendi praesentium! Dolorem atque eveniet magnam vero
-              voluptatum inventore repellendus error totam nisi molestiae illum
-              enim unde voluptatem commodi deserunt, officiis quo tempora?
-              Delectus sapiente fuga accusantium voluptatem culpa eligendi esse
-              similique quidem sunt labore provident modi, officia, doloribus
-              vel quod earum eius. Deleniti eveniet aliquam, consequatur velit
-              quas earum impedit officiis quam provident nisi ab autem similique
-              architecto explicabo voluptas at beatae, qui distinctio omnis
-              assumenda obcaecati magnam? Nobis neque, suscipit voluptate
-              molestiae laudantium odit vero?
-            </p>
           </motion.div>
         )}
       </div>
 
-      <h1>{loaderData.name}</h1>
-      <p>{loaderData.description}</p>
-      <img src={loaderData.imagePath} alt={loaderData.name} />
+      <div className="header">
+        <div className="image-container">
+          <img src="/placeholder.jpg" alt="Placeholder" />
+        </div>
+
+        <h1>Elektrotehnicar informacionih tehnologija</h1>
+      </div>
+
+      <div className="body">
+        <p>
+          Elektrotehničar informacionih tehnologija je savremeni obrazovni
+          profil osmišljen za učenike koji žele da se specijalizuju u oblasti
+          informacionih tehnologija, koja predstavlja temelj savremenog
+          digitalnog društva i jedan od najperspektivnijih sektora u savremenom
+          svetu. Ovaj obrazovni profil omogućava učenicima da steknu širok
+          spektar znanja i praktičnih veština neophodnih za rad u IT industriji,
+          kao i za nastavak obrazovanja na visokoškolskim ustanovama tehničkog
+          usmerenja.
+        </p>
+
+        <h2>Mogućnosti nakon školovanja</h2>
+
+        <p>
+          Po završetku školovanja, učenici su spremni da nastave svoje
+          obrazovanje na fakultetima koji se bave informacionim tehnologijama,
+          softverskim inženjeringom, računarstvom i srodnim oblastima. Takođe,
+          stiču kompetencije koje im omogućavaju da odmah započnu karijeru u IT
+          industriji na pozicijama poput junior programera, mrežnog tehničara,
+          administratora baza podataka, tehničke podrške, kao i mnogim drugim
+          ulogama u oblasti informacionih tehnologija.
+        </p>
+
+        <h2>Obrazovni program i stečene veštine</h2>
+
+        <p>
+          Tokom četvorogodišnjeg školovanja, učenici se upoznavaju sa širokim
+          spektrom znanja i veština iz oblasti programiranja, mrežnih
+          tehnologija, baza podataka i računarskog hardvera. Ova znanja
+          uključuju:
+        </p>
+
+        <ul className="skills">
+          <li>
+            <p> Programiranje i razvoj softvera</p>
+            <p>
+              Rad sa savremenim programskim jezicima, izrada desktop i web
+              aplikacija, objektno orijentisano programiranje
+            </p>
+          </li>
+
+          <li>
+            <p> Veb tehnologije</p>
+            <p>
+              Kreiranje statičkih i dinamičkih veb stranica, korišćenje HTML,
+              CSS, JavaScript i naprednih frontend i backend tehnologija
+            </p>
+          </li>
+
+          <li>
+            <p> Baze podataka</p>
+            <p>
+              Dizajn, implementacija i administracija baza podataka, rad sa SQL
+              i NoSQL bazama
+            </p>
+          </li>
+
+          <li>
+            <p> Mrežne tehnologije</p>
+            <p>
+              Projektovanje, konfiguracija i održavanje računarskih mreža, rad
+              sa mrežnim protokolima i sigurnosnim sistemima
+            </p>
+          </li>
+
+          <li>
+            <p> Računarski hardver</p>
+            <p>
+              Razumevanje principa rada računarskih komponenti i uređaja,
+              sastavljanje i održavanje računarskih sistema
+            </p>
+          </li>
+        </ul>
+
+        <p>
+          Osim tehničkih znanja, učenici razvijaju ključnesoft skills veštine,
+          kao što su analitičko razmišljanje, sposobnost rešavanja problema,
+          timski rad i prilagodljivost – osobine koje su izuzetno važne za uspeh
+          u dinamičnom svetu informacionih tehnologija.
+        </p>
+
+        <h2>Nastava i praktična primena znanja</h2>
+
+        <p>
+          Nastavni proces kombinuje teorijska predavanja i praktičan rad u
+          savremeno opremljenim kabinetima i laboratorijama, čime se učenicima
+          omogućava da stečeno znanje odmah primenjuju kroz realne projekte i
+          zadatke. Praktična nastava i stručna praksa dodatno pripremaju učenike
+          za rad u realnom okruženju, kroz saradnju sa IT kompanijama i
+          institucijama.
+        </p>
+
+        <h2>Za koga je ovaj obrazovni profil?</h2>
+
+        <p>
+          Elektrotehničar informacionih tehnologija je idealan izbor za sve one
+          koji žele da se bave modernim tehnologijama, vole rešavanje problema i
+          žele da budu deo jednog od najbrže rastućih sektora današnjice. Bilo
+          da planiraju nastavak studija ili žele da odmah započnu karijeru u IT
+          industriji, ovaj obrazovni profil pruža stabilnu osnovu za
+          profesionalni razvoj i uspeh.
+        </p>
+      </div>
 
       <div className="classes-container">
         <div className="year-selector">
