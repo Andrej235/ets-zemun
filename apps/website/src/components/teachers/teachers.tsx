a7797f26e454fa2940be47ce4f8f9c3d54dff880
Andrej235 <andrej.nenadic06@gmail.com>
2025-02-04 21:29:49 +0100

    Create hero section of teachers page

--- apps/website/src/components/teachers/teachers.tsx
+++ apps/website/src/components/teachers/teachers.tsx
@@ -1,6 +1,50 @@
-import WipWall from "../wip-wall/wip-wall";
+import { animate } from "motion";
+import { useEffect, useState } from "react";
+import "./teachers.scss";
 
 export default function Teachers() {
-  return <WipWall />;
+  return (
+    <div className="teachers-page">
+      <div className="image-container">
+        <img src="/images/info-cards/teachers.jpg" alt="Our team of teachers" />
+      </div>
+
+      <div className="info">
+        <h1>Vodimo buduće inovatore u elektrotehnici</h1>
+
+        <div className="cards">
+          <Card count={100} title="Profesora" />
+          <Card count={1000} title="Godina iskustva" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Card({
+  count,
+  title,
+}: {
+  readonly count: number;
+  readonly title: string;
+}) {
+  const [currentCount, setCurrentCount] = useState(0);
+
+  useEffect(() => {
+    animate(0, count, {
+      duration: 1.5,
+      ease: "easeOut",
+      onUpdate: (latest) => {
+        setCurrentCount(Math.floor(latest));
+      },
+    });
+  }, [count]);
+
+  return (
+    <div className="card">
+      <h2>{currentCount}+</h2>
+      <p>{title}</p>
+    </div>
+  );
 }
 
