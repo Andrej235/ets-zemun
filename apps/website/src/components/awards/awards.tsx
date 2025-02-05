e35eff3c04feeaa5b437b885e0cc933d337a23d1
Aleksa Andjelic <23151811a@gmail.com>
2025-02-05 17:50:23 +0100

    Create awards-list in Awards page

--- apps/website/src/components/awards/awards.tsx
+++ apps/website/src/components/awards/awards.tsx
@@ -1,4 +1,5 @@
 import "./awards.scss";
+import { awards } from "./mock-awards-data";
 
 export default function Awards() {
   return (
@@ -7,8 +8,30 @@ export default function Awards() {
         <div className="image-container">
           <img src="/images/awards-hero.jpg" alt="students holding diplomas" />
         </div>
-
         <h1>Zlatni Standard: 25+ Nagrada kao Dokaz Kvaliteta!</h1>
+      </div>
+      <div className="awards-list">
+        {awards.map((award) => (
+          <div key={award.id} className="award-card">
+            <img src={award.image} alt={award.title} />
+            <h2>{award.title}</h2>
+            <p>
+              {award.competition} - {award.year}
+            </p>
+            <p>{award.category}</p>
+            <p>{award.description}</p>
+            {award.projectSummary && <p>{award.projectSummary}</p>}
+            {award.externalLink && (
+              <a
+                href={award.externalLink}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn more
+              </a>
+            )}
+          </div>
+        ))}
       </div>
     </div>
   );
