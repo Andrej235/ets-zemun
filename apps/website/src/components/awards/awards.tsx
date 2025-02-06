5dee18b4ca6b064c3474045398c9cf239e5fed98
Aleksa Andjelic <23151811a@gmail.com>
2025-02-06 13:01:33 +0100

    Implement externalLink on award-card in Awards component

--- apps/website/src/components/awards/awards.tsx
+++ apps/website/src/components/awards/awards.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router";
 import "./awards.scss";
 import { awards } from "./mock-awards-data";
 import { PointerEvent } from "react";
@@ -25,24 +26,26 @@ export default function Awards() {
       </div>
       <div className="awards-list">
         {awards.map((award) => (
-          <div
-            key={award.id}
-            className="award-card"
-            onPointerMove={handleMouseMove}
-          >
-            <img src={award.image} alt={award.title} />
-            <div className="award-card-header">
-              <h2>{award.title}</h2>
-              <p>
-                {award.competition} - {award.year}
-              </p>
+          <Link to={award.externalLink ?? "/takmicenja"} key={award.id}>
+            <div
+              key={award.id}
+              className="award-card"
+              onPointerMove={handleMouseMove}
+            >
+              <img src={award.image} alt={award.title} />
+              <div className="award-card-header">
+                <h2>{award.title}</h2>
+                <p>
+                  {award.competition} - {award.year}
+                </p>
+              </div>
+              <div className="content">
+                <p>Kategorija:{" " + award.category}</p>
+                {award.projectSummary && <p>{award.projectSummary}</p>}
+                <p>{award.description}</p>
+              </div>
             </div>
-            <div className="content">
-              <p>Kategorija:{" " + award.category}</p>
-              {award.projectSummary && <p>{award.projectSummary}</p>}
-              <p>{award.description}</p>
-            </div>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
