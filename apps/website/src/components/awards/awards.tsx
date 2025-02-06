a7b7bd886b1e4cce9f969e7a3c5b72e587fa5f4c
Aleksa Andjelic <23151811a@gmail.com>
2025-02-06 12:54:58 +0100

    Create handleMouseMove function for Awards component

--- apps/website/src/components/awards/awards.tsx
+++ apps/website/src/components/awards/awards.tsx
@@ -1,7 +1,20 @@
 import "./awards.scss";
 import { awards } from "./mock-awards-data";
+import { PointerEvent } from "react";
 
 export default function Awards() {
+  const handleMouseMove = (e: PointerEvent) => {
+    if (e.pointerType !== "mouse") return;
+
+    const target = e.currentTarget as HTMLAnchorElement;
+    const { top, left } = target.getBoundingClientRect();
+    const x = e.clientX - left;
+    const y = e.clientY - top;
+
+    target.style.setProperty("--mouse-x", `${x}px`);
+    target.style.setProperty("--mouse-y", `${y}px`);
+  };
+
   return (
     <div className="awards-pages">
       <div className="hero">
@@ -12,7 +25,11 @@ export default function Awards() {
       </div>
       <div className="awards-list">
         {awards.map((award) => (
-          <div key={award.id} className="award-card">
+          <div
+            key={award.id}
+            className="award-card"
+            onPointerMove={handleMouseMove}
+          >
             <img src={award.image} alt={award.title} />
             <div className="award-card-header">
               <h2>{award.title}</h2>
