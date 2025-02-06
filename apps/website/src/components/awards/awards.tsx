5c21577e5557a6227e90627ca1ab2b577141a9cd
Aleksa Andjelic <23151811a@gmail.com>
2025-02-06 13:09:51 +0100

    Delete hero-space form Awards component

--- apps/website/src/components/awards/awards.tsx
+++ apps/website/src/components/awards/awards.tsx
@@ -18,12 +18,7 @@ export default function Awards() {
 
   return (
     <div className="awards-pages">
-      <div className="hero">
-        <div className="image-container">
-          <img src="/images/awards-hero.jpg" alt="students holding diplomas" />
-        </div>
-        <h1>Zlatni Standard: 25+ Nagrada kao Dokaz Kvaliteta!</h1>
-      </div>
+      <h1>Takmicenja i nagrade</h1>
       <div className="awards-list">
         {awards.map((award) => (
           <Link to={award.externalLink ?? "/takmicenja"} key={award.id}>
