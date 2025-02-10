b0eb1c96f000d6f8e9f8cf80eca9c41186b62cf4
Aleksa Andjelic <23151811a@gmail.com>
2025-02-10 14:44:55 +0100

    Create table on Enrollment page

--- apps/website/src/components/enrollment/enrollment.tsx
+++ apps/website/src/components/enrollment/enrollment.tsx
@@ -1,6 +1,33 @@
 import Icon from "@components/icon/icon";
 import "./enrollment.scss";
 
+const programs = [
+  {
+    name: "Elektrotehničar računara",
+    duration: "4 godine",
+    students: 90,
+    requiredPoints: 85,
+  },
+  {
+    name: "Elektronika",
+    duration: "4 godine",
+    students: 60,
+    requiredPoints: 78,
+  },
+  {
+    name: "Telekomunikacije",
+    duration: "3 godine",
+    students: 30,
+    requiredPoints: 75,
+  },
+  {
+    name: "Energetika",
+    duration: "4 godine",
+    students: 45,
+    requiredPoints: 80,
+  },
+];
+
 export default function Enrollment() {
   return (
     <div className="enrollment-page">
@@ -9,7 +36,7 @@ export default function Enrollment() {
 
       <div className="timeline">
         <div className="card">
-          <h1>21.03. и 22.03.2025. године</h1>
+          <h1>21.03. i 22.03.2025. године</h1>
           <p>Пробни завршни испит</p>
         </div>
 
@@ -26,6 +53,32 @@ export default function Enrollment() {
         <div className="card">
           <h1>28.06.2025.</h1>
           <p>Објављивање коначних резултата расподеле по средњим школама</p>
+        </div>
+      </div>
+
+      <div className="enrollment-table">
+        <div className="card">
+          <table>
+            <caption>Programi upisa</caption>
+            <thead>
+              <tr>
+                <th>Smer</th>
+                <th>Trajanje</th>
+                <th>Broj učenika</th>
+                <th>Potrebni bodovi</th>
+              </tr>
+            </thead>
+            <tbody>
+              {programs.map((program, index) => (
+                <tr key={index + ""}>
+                  <td>{program.name}</td>
+                  <td>{program.duration}</td>
+                  <td>{program.students}</td>
+                  <td>{program.requiredPoints}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
 
