48f78b016fe2a732287665d5097a5cc935cd4e4a
Andrej235 <andrej.nenadic06@gmail.com>
2025-02-12 19:04:59 +0100

    Fix backend's cors policy disallowing admin panel from making api requests

--- apps/admin-panel/src/components/about/about.tsx
+++ apps/admin-panel/src/components/about/about.tsx
@@ -3,6 +3,12 @@ import sendAPIRequest from "@shared/api-dsl/send-api-request";
 export default function About() {
   return (
     <div className="about-page">
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
       <button
         onClick={async () => {
           const response = await sendAPIRequest("/auth", {
@@ -10,7 +16,9 @@ export default function About() {
           });
           console.log(response);
         }}
-      ></button>
+      >
+        Get auth claims
+      </button>
     </div>
   );
 }
