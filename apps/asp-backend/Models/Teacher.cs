39c78ca04adeb942fe2d7dbae8fb82c62fd41fc5
Andrej235 <andrej.nenadic06@gmail.com>
2025-02-12 22:25:13 +0100

    Change type of teacher's open office hours DateTime to TimeOnly

--- apps/asp-backend/Models/Teacher.cs
+++ apps/asp-backend/Models/Teacher.cs
@@ -5,8 +5,8 @@ public class Teacher
     public int Id { get; set; }
     public ICollection<TeacherTranslation> Translations { get; set; } = [];
 
-    public DateTime StartOfOpenOfficeHoursFirstShift { get; set; }
-    public DateTime StartOfOpenOfficeHoursSecondShift { get; set; }
+    public TimeOnly? StartOfOpenOfficeHoursFirstShift { get; set; }
+    public TimeOnly? StartOfOpenOfficeHoursSecondShift { get; set; }
 
     public ICollection<Qualification> Qualifications { get; set; } = [];
     public ICollection<Subject> Subjects { get; set; } = [];
