5104a30a89544d1ccf3f85ea7010c2b726b13b21
Andrej235 <andrej.nenadic06@gmail.com>
2025-02-17 15:36:40 +0100

    Translate student page's part time section

--- apps/website/src/components/students/students-page-part-time.tsx
+++ apps/website/src/components/students/students-page-part-time.tsx
@@ -1,148 +1,69 @@
+import { useMemo } from "react";
+import { useTranslation } from "react-i18next";
+
+type Deadlines = {
+  term: string;
+  registration: string;
+  exam: string;
+}[];
+
+type PriceLists = {
+  itemName: string;
+  itemPrice: string;
+}[];
+
+type ExamData = {
+  subject: string;
+  commission: string[];
+  date: string;
+  time: string;
+  cabinet: string;
+}[];
+
 export default function StudentsPagePartTime() {
-  const deadlines = [
-    {
-      term: "OKTOBARSKI",
-      registration: "09-27.09.2024.",
-      exam: "07-31.10.2024.",
-    },
-    {
-      term: "DECEMBARSKI",
-      registration: "04-22.11.2024.",
-      exam: "02-27.12.2024.",
-    },
-    {
-      term: "MARTOVSKI",
-      registration: "03-21.02.2025.",
-      exam: "03-28.03.2025.",
-    },
-    {
-      term: "MAJSKI",
-      registration: "01-25.04.2025.",
-      exam: "12-30.05.2025.",
-    },
-    {
-      term: "AVGUSTOVSKI",
-      registration: "02-20.06.2025.",
-      exam: "18-29.08.2025.",
-    },
-  ];
+  const { t } = useTranslation();
 
-  const priceList = [
-    { itemName: "UPIS GODINE", itemPrice: "7.500,00 din." },
-    { itemName: "OBNOVA GODINE", itemPrice: "1.500,00 din." },
-    { itemName: "ISPIT SA PISMENIM RADOM", itemPrice: "1.000,00 din." },
-    { itemName: "ISPIT BEZ PISMENOG", itemPrice: "1.000,00 din." },
-    { itemName: "ISPIT IZ PRAKTIČNE NASTAVE", itemPrice: "1.000,00 din." },
-    { itemName: "ISPIT SA VEŽBAMA", itemPrice: "1.000,00 din." },
-    { itemName: "ZAVRŠNI ISPIT", itemPrice: "2.500,00 din." },
-    { itemName: "MATURSKI ISPIT", itemPrice: "3.000,00 din." },
-  ];
+  const deadlines: Deadlines = useMemo(
+    () =>
+      t("students.sections.partTime.deadlines", {
+        returnObjects: true,
+      }) as Deadlines,
+    [t]
+  );
 
-  const examData = [
-    {
-      subject: "SRPSKI JEZIK-maturski",
-      commission: [
-        "Srđan Gagić",
-        "Nada Đurić",
-        "Marina Ristanović",
-        "Vurdelja Dejan",
-        "Radmila Vidović",
-      ],
-      date: "Utorak, 28.01.2025.",
-      time: "14.45, 10.30",
-      cabinet: "17 ili 18",
-    },
-    {
-      subject: "STRUČNO TEORIJSKI ISPIT (ARM, IT, ER)",
-      commission: ["Bogdan Luković", "Anđela Pavlović"],
-      date: "Sreda, 29.01.2025.",
-      time: "10.00",
-      cabinet: "14",
-    },
-    {
-      subject: "STRUČNO TEORIJSKI ISPIT (ARM, IT, ER)",
-      commission: ["Slavica Per", "Relja Ćurčin", "Vesna Janjić"],
-      date: "Sreda, 29.01.2025.",
-      time: "15.35",
-      cabinet: "14",
-    },
-    {
-      subject: "PRAKTIČNI ISPIT-ARM-A",
-      commission: ["Slavica Per", "Saša Stošić"],
-      date: "Četvrtak, 30.01.2025.",
-      time: "11.15",
-      cabinet: "44",
-    },
-    {
-      subject: "PRAKTIČNI ISPIT-ARM-B",
-      commission: ["Slavica Per", "Saša Stošić"],
-      date: "petak, 31.01.2025.",
-      time: "13.00",
-      cabinet: "43",
-    },
-    {
-      subject: "PRAKTIČNI ISPIT-ER-A",
-      commission: ["Radovan Đurić", "Saviša Nikolić"],
-      date: "Četvrtak, 30.01.2025.",
-      time: "14.00",
-      cabinet: "43",
-    },
-    {
-      subject: "PRAKTIČNI ISPIT-ER-B",
-      commission: ["Radovan Đurić", "Slaviša Nikolić"],
-      date: "petak, 31.01.2025.",
-      time: "14.45",
-      cabinet: "43",
-    },
-    {
-      subject: "PRAKTIČNI ISPIT-IT-A",
-      commission: ["Galina Bojović", "Relja Ćurčin"],
-      date: "Četvrtak, 30.01.2025.",
-      time: "11.25",
-      cabinet: "50",
-    },
-    {
-      subject: "PRAKTIČNI ISPIT-IT-B",
-      commission: ["Galina Bojović", "Relja Ćurčin"],
-      date: "petak, 31.01.2025.",
-      time: "12.10",
-      cabinet: "50",
-    },
-    {
-      subject: "SERVISER TERMIČKIH I RASHLADNIH UREĐAJA",
-      commission: ["Branko Lončar", "Mavrak Milenko"],
-      date: "31.01.2025.",
-      time: "14.00",
-      cabinet: "73",
-    },
-    {
-      subject: "MATURSKI ISPIT (elektrotehničar automatike)",
-      commission: ["Cvija Jelenković", "Vesna Janjić", "Branislav Siljković"],
-      date: "30.01.2025., 31.01.2025.",
-      time: "13.00, 14.00",
-      cabinet: "40",
-    },
-  ];
+  const priceList: PriceLists = useMemo(
+    () =>
+      t("students.sections.partTime.priceList", {
+        returnObjects: true,
+      }) as PriceLists,
+    [t]
+  );
+
+  const examData: ExamData = useMemo(
+    () =>
+      t("students.sections.partTime.examData", {
+        returnObjects: true,
+      }) as ExamData,
+    [t]
+  );
 
   return (
     <div className="part-time-container">
-      <h1>Vanredni učenici</h1>
-      
+      <h1>{t("students.sections.partTime.title")}</h1>
+
       <div className="table-container">
         <table>
-          <caption>
-            ROKOVI ZA VANREDNE UČENIKE U ŠKOLSKOJ 2024/2025.GODINI
-          </caption>
+          <caption>{t("students.sections.partTime.deadlinesCaption")}</caption>
           <thead>
             <tr>
-              <th>ROKOVI</th>
-              <th>PRIJAVLJIVANJA</th>
-              <th>POLAGANJA</th>
+              <th>{t("students.sections.partTime.deadlinesHeaders.0")}</th>
+              <th>{t("students.sections.partTime.deadlinesHeaders.1")}</th>
+              <th>{t("students.sections.partTime.deadlinesHeaders.2")}</th>
             </tr>
           </thead>
           <tbody>
-            {deadlines.map((deadline, index) => (
-              <tr key={index + ""}>
+            {deadlines.map((deadline) => (
+              <tr key={deadline.term}>
                 <td>{deadline.term}</td>
                 <td>{deadline.registration}</td>
                 <td>{deadline.exam}</td>
@@ -153,9 +74,9 @@ export default function StudentsPagePartTime() {
       </div>
 
       <div className="price-list">
-        <h2>CENOVNIK VANREDNOG ŠKOLOVANjA ZA ŠKOLSKU 2024/2025.GODINU</h2>
-        {priceList.map((item, index) => (
-          <div className="price-list-item" key={index + ""}>
+        <h2>{t("students.sections.partTime.priceListTitle")}</h2>
+        {priceList.map((item) => (
+          <div className="price-list-item" key={item.itemName}>
             <div className="item-name">{item.itemName}</div>
             <div className="item-price">{item.itemPrice}</div>
           </div>
@@ -163,30 +84,28 @@ export default function StudentsPagePartTime() {
       </div>
 
       <div className="payment-method">
-        <h2>RAČUN ZA UPLATU ZA VANREDNE UČENIKE</h2>
+        <h2>{t("students.sections.partTime.paymentMethod.title")}</h2>
         <p>840-31302845-09</p>
-        <p>OBAVEZNO:</p>
-        <p>POZIV NA BROJ:</p>
+        <p>{t("students.sections.partTime.paymentMethod.description.0")}:</p>
+        <p>{t("students.sections.partTime.paymentMethod.description.1")}</p>
         <p>97 06601832040174231700</p>
       </div>
 
       <div className="table-container">
         <table className="part-time-table">
-          <caption>
-            MATURSKI/ZAVRŠNI ISPIT U DECEMBARSKO/JANUARSKOM ROKU
-          </caption>
+          <caption>{t("students.sections.partTime.examDataCaption")}</caption>
           <thead>
             <tr>
-              <th>PREDMET</th>
-              <th>KOMISIJA</th>
-              <th>DATUM POLAGANjA</th>
-              <th>VREME POLAGANjA</th>
-              <th>KABINET</th>
+              <th>{t("students.sections.partTime.examDataHeaders.0")}</th>
+              <th>{t("students.sections.partTime.examDataHeaders.1")}</th>
+              <th>{t("students.sections.partTime.examDataHeaders.2")}</th>
+              <th>{t("students.sections.partTime.examDataHeaders.3")}</th>
+              <th>{t("students.sections.partTime.examDataHeaders.4")}</th>
             </tr>
           </thead>
           <tbody>
-            {examData.map((exam, index) => (
-              <tr key={index + ""}>
+            {examData.map((exam) => (
+              <tr key={exam.commission + exam.subject}>
                 <td>{exam.subject}</td>
                 <td>{exam.commission.join(", ")}</td>
                 <td>{exam.date}</td>
