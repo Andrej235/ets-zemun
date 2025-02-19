e508395fcba94804ac645c41e14c889b589a6bbf
Andrej235 <andrej.nenadic06@gmail.com>
2025-02-20 00:19:05 +0100

    Create a popover asking the user to confirm before saving a news article

--- apps/admin-panel/src/components/news/news.tsx
+++ apps/admin-panel/src/components/news/news.tsx
@@ -2,7 +2,9 @@ import { useQuill } from "react-quilljs";
 //@ts-expect-error CSS
 import "quill/dist/quill.snow.css";
 import "./news.scss";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { Button } from "../ui/button";
 
 export default function News() {
   const { quillRef, quill } = useQuill();
@@ -16,17 +18,38 @@ export default function News() {
     });
   }, [quill]);
 
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
     <div className="news">
       <div ref={quillRef} />
 
-      <button
-        onClick={() => {
-          console.log(quill?.root.innerHTML);
-        }}
-      >
-        Save
-      </button>
+      <Popover open={isModalOpen} onOpenChange={setIsModalOpen}>
+        <PopoverTrigger>Sacuvaj</PopoverTrigger>
+        <PopoverContent className="flex flex-col justify-center gap-3 p-12 min-w-max rounded-2xl">
+          <h1 className="w-max font-bold text-3xl">
+            Da li ste sigurni da zelite da sacuvate promene?
+          </h1>
+          <h2 className="w-max text-2xl text-muted-foreground">
+            Ova akcija je nepovratna
+          </h2>
+
+          <div className="flex justify-end gap-3">
+            <Button variant="outline" onClick={() => setIsModalOpen(false)}>
+              Odustani
+            </Button>
+            <Button
+              onClick={() => {
+                console.log(quill?.root.innerHTML);
+                localStorage.removeItem("draft");
+                setIsModalOpen(false);
+              }}
+            >
+              Sacuvaj
+            </Button>
+          </div>
+        </PopoverContent>
+      </Popover>
     </div>
   );
 }
