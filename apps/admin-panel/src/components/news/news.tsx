201bd43754e02e717808b76370aae5b0a9ec9626
Andrej235 <andrej.nenadic06@gmail.com>
2025-02-19 23:18:03 +0100

    Persist changes to a news article by saving all changes as a draft in local storage

--- apps/admin-panel/src/components/news/news.tsx
+++ apps/admin-panel/src/components/news/news.tsx
@@ -2,13 +2,31 @@ import { useQuill } from "react-quilljs";
 //@ts-expect-error CSS
 import "quill/dist/quill.snow.css";
 import "./news.scss";
+import { useEffect } from "react";
 
 export default function News() {
-  const { quillRef } = useQuill();
+  const { quillRef, quill } = useQuill();
+
+  useEffect(() => {
+    const draft = localStorage.getItem("draft");
+    if (draft && quill) quill.root.innerHTML = draft;
+
+    quill?.on("text-change", () => {
+      localStorage.setItem("draft", quill?.root.innerHTML);
+    });
+  }, [quill]);
 
   return (
     <div className="news">
       <div ref={quillRef} />
+
+      <button
+        onClick={() => {
+          console.log(quill?.root.innerHTML);
+        }}
+      >
+        Save
+      </button>
     </div>
   );
 }
