311adfd1064398aaa0e30f286f4707e6285327a4
Andrej235 <andrej.nenadic06@gmail.com>
2025-02-20 12:37:14 +0100

    Compress all images uploaded during a news article creation

--- apps/admin-panel/src/components/news/news.tsx
+++ apps/admin-panel/src/components/news/news.tsx
@@ -5,20 +5,100 @@ import "./news.scss";
 import { useEffect, useState } from "react";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 import { Button } from "../ui/button";
+import Toolbar from "quill/modules/toolbar";
 
 export default function News() {
   const { quillRef, quill } = useQuill();
 
   useEffect(() => {
-    const draft = localStorage.getItem("draft");
-    if (draft && quill) quill.root.innerHTML = draft;
+    if (!quill) return;
 
-    quill?.on("text-change", () => {
-      localStorage.setItem("draft", quill?.root.innerHTML);
+    const draft = localStorage.getItem("draft");
+    if (draft) quill.root.innerHTML = draft;
+
+    const insertToEditor = (url: string) => {
+      const range = quill.getSelection();
+      if (!range) return;
+      quill.insertEmbed(range.index, "image", url);
+    };
+
+    const compressImage = async (file: File, quality = 1) => {
+      // Get as image data
+      const imageBitmap = await createImageBitmap(file);
+
+      // Draw to canvas
+      const canvas = document.createElement("canvas");
+      canvas.width = imageBitmap.width;
+      canvas.height = imageBitmap.height;
+      const ctx = canvas.getContext("2d")!;
+      ctx.drawImage(imageBitmap, 0, 0);
+
+      // Turn into Blob
+      const blob = await new Promise<Blob | null>((resolve) =>
+        canvas.toBlob(resolve, "image/jpeg", quality)
+      );
+
+      // Turn Blob into File
+      return new File([blob!], file.name, {
+        type: blob!.type,
+      });
+    };
+
+    const addImage = async (file: File) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(await compressImage(file, 0.5));
+      reader.onload = () => {
+        const imageDataUrl = reader.result as string;
+        insertToEditor(imageDataUrl);
+      };
+    };
+
+    const toolbar = quill.getModule("toolbar") as Toolbar;
+    toolbar.addHandler("image", (x) => {
+      const input = document.createElement("input");
+      input.setAttribute("type", "file");
+      input.setAttribute("accept", "image/*");
+      input.click();
+
+      input.onchange = () => {
+        const file = input.files![0];
+        addImage(file);
+      };
+      console.log(x);
+    });
+
+    quill.on("text-change", () => {
+      if (quill) localStorage.setItem("draft", quill.root.innerHTML);
     });
   }, [quill]);
 
   const [isModalOpen, setIsModalOpen] = useState(false);
+
+  function handleSave() {
+    if (!quill) return;
+
+    const root = quill.root;
+    const images = root.querySelectorAll("img");
+    const imageSources: {
+      source: string;
+      id: number;
+    }[] = [];
+
+    images.forEach((image, i) => {
+      const src = image.getAttribute("src");
+      if (src)
+        imageSources.push({
+          id: i + 1,
+          source: src,
+        });
+      image.setAttribute("src", "");
+      image.id = "image_" + (i + 1);
+    });
+
+    console.log(quill.root.innerHTML, imageSources);
+    localStorage.removeItem("draft");
+    setIsModalOpen(false);
+  }
 
   return (
     <div className="news">
@@ -38,15 +118,7 @@ export default function News() {
             <Button variant="outline" onClick={() => setIsModalOpen(false)}>
               Odustani
             </Button>
-            <Button
-              onClick={() => {
-                console.log(quill?.root.innerHTML);
-                localStorage.removeItem("draft");
-                setIsModalOpen(false);
-              }}
-            >
-              Sacuvaj
-            </Button>
+            <Button onClick={handleSave}>Sacuvaj</Button>
           </div>
         </PopoverContent>
       </Popover>
