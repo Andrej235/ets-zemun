bfe87cb2dc448d3fca1ca94eb6a18872b4623a50
Andrej235 <andrej.nenadic06@gmail.com>
2025-02-20 13:14:02 +0100

    Add input fields for news preview on admin panel news creation

---
+++ apps/admin-panel/src/components/news/news-preview.tsx
@@ -0,0 +1,47 @@
+import { PointerEvent } from "react";
+import "./news-preview.scss";
+
+type NewsPreviewProps = {
+  readonly date: Date;
+  readonly title: string;
+  readonly description: string;
+  readonly image: string;
+};
+
+export default function NewsPreview({
+  date,
+  title,
+  description,
+  image,
+}: NewsPreviewProps) {
+  const handleMouseMove = (e: PointerEvent) => {
+    if (e.pointerType !== "mouse") return;
+
+    const target = e.currentTarget as HTMLAnchorElement;
+    const { top, left } = target.getBoundingClientRect();
+    const x = e.clientX - left;
+    const y = e.clientY - top;
+
+    target.style.setProperty("--mouse-x", `${x}px`);
+    target.style.setProperty("--mouse-y", `${y}px`);
+  };
+
+  return (
+    <a
+      href=" "
+      className="news-article-preview"
+      onPointerMove={handleMouseMove}
+    >
+      <div className="image-container">
+        <img src={image} alt={title} />
+      </div>
+      <div className="info">
+        <h1 className="title">{title}</h1>
+        <br />
+        <p className="description">{description}</p>
+        <p className="date">{date.toLocaleDateString()}</p>
+      </div>
+    </a>
+  );
+}
+
--- apps/admin-panel/src/components/news/news.tsx
+++ apps/admin-panel/src/components/news/news.tsx
@@ -6,6 +6,9 @@ import { useEffect, useState } from "react";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 import { Button } from "../ui/button";
 import Toolbar from "quill/modules/toolbar";
+import { Input } from "../ui/input";
+import { Textarea } from "../ui/textarea";
+import { DatePicker } from "../ui/date-picker";
 
 export default function News() {
   const { quillRef, quill } = useQuill();
@@ -101,27 +104,38 @@ export default function News() {
   }
 
   return (
-    <div className="news">
-      <div ref={quillRef} />
+    <div className="w-full h-max p-20 flex flex-col gap-20">
+      <div className="w-full h-max flex gap-10">
+        <div className="w-full flex flex-col gap-3">
+          <Input placeholder="Naslov" className="pt-5 pb-5 h-10" />
+          <Textarea placeholder="Kratak opis" />
+          <Input type="file" />
+          <DatePicker />
+        </div>
+      </div>
 
-      <Popover open={isModalOpen} onOpenChange={setIsModalOpen}>
-        <PopoverTrigger>Sacuvaj</PopoverTrigger>
-        <PopoverContent className="flex flex-col justify-center gap-3 p-12 min-w-max rounded-2xl">
-          <h1 className="w-max font-bold text-3xl">
-            Da li ste sigurni da zelite da sacuvate promene?
-          </h1>
-          <h2 className="w-max text-2xl text-muted-foreground">
-            Ova akcija je nepovratna
-          </h2>
+      <div className="w-full h-[100vh]">
+        <div ref={quillRef} />
 
-          <div className="flex justify-end gap-3">
-            <Button variant="outline" onClick={() => setIsModalOpen(false)}>
-              Odustani
-            </Button>
-            <Button onClick={handleSave}>Sacuvaj</Button>
-          </div>
-        </PopoverContent>
-      </Popover>
+        <Popover open={isModalOpen} onOpenChange={setIsModalOpen}>
+          <PopoverTrigger>Sacuvaj</PopoverTrigger>
+          <PopoverContent className="flex flex-col justify-center gap-3 p-12 min-w-max rounded-2xl">
+            <h1 className="w-max font-bold text-3xl">
+              Da li ste sigurni da zelite da sacuvate promene?
+            </h1>
+            <h2 className="w-max text-2xl text-muted-foreground">
+              Ova akcija je nepovratna
+            </h2>
+
+            <div className="flex justify-end gap-3">
+              <Button variant="outline" onClick={() => setIsModalOpen(false)}>
+                Odustani
+              </Button>
+              <Button onClick={handleSave}>Sacuvaj</Button>
+            </div>
+          </PopoverContent>
+        </Popover>
+      </div>
     </div>
   );
 }
