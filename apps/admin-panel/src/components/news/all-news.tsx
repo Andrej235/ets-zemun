6166985d779918ccb37925668a226212adefa326
Andrej235 <andrej.nenadic06@gmail.com>
2025-02-20 20:21:32 +0100

    Adjust layout of all news admin panel component

--- apps/admin-panel/src/components/news/all-news.tsx
+++ apps/admin-panel/src/components/news/all-news.tsx
@@ -7,8 +7,8 @@ export default function AllNews() {
   const loaderData = useLoader<typeof newsLoader>();
 
   return (
-    <div className="max-w-full h-max overflow-auto">
-      <div className="w-max h-max p-10 flex gap-8 overflow-auto">
+    <div className="max-w-full h-max overflow-auto flex justify-center">
+      <div className="w-max h-max p-10 grid grid-cols-3 gap-8 overflow-auto">
         <LazyAwaitedList data={loaderData} success="OK">
           {(news) => (
             <NewsPreview
