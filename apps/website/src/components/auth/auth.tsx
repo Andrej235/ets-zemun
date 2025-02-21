626307373c7ef0e2871f99bda2819bc751aa50ef
Aleksa Andjelic <23151811a@gmail.com>
2025-02-21 14:41:47 +0100

    Create structure for Auth component

--- apps/website/src/components/auth/auth.tsx
+++ apps/website/src/components/auth/auth.tsx
@@ -1,3 +1,64 @@
+import { useState } from "react";
+import "./auth.scss";
+
 export default function Auth() {
-  return <div>Auth</div>;
+  const [active, setActive] = useState(false);
+
+  const handleRegisterClick = () => {
+    setActive(true);
+  };
+
+  const handleLoginClick = () => {
+    setActive(false);
+  };
+
+  return (
+    <div className="auth-page">
+      <div className={`auth-container ${active ? "active" : ""}`} id="container">
+        <div className="form-container sign-up">
+          <form>
+            <h1>Create Account</h1>
+            <span>or use your email for registration</span>
+            <input type="text" placeholder="Name" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <button>Sign Up</button>
+          </form>
+        </div>
+
+        <div className="form-container sign-in">
+          <form>
+            <h1>Sign In</h1>
+            <span>or use your email password</span>
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <a href="/">Forget Your Password?</a>
+            <button>Sign In</button>
+          </form>
+        </div>
+
+        <div className="toggle-container">
+          <div className="toggle">
+            <div className="toggle-panel toggle-left">
+              <h1>Welcome Back!</h1>
+              <p>Enter your personal details to use all of site features</p>
+              <button className="hidden" onClick={handleLoginClick}>
+                Sign In
+              </button>
+            </div>
+            <div className="toggle-panel toggle-right">
+              <h1>Hello, Friend!</h1>
+              <p>
+                Register with your personal details to use all of site features
+              </p>
+              <button className="hidden" onClick={handleRegisterClick}>
+                Sign Up
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
+
