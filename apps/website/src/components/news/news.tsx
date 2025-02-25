f6d0b1f8ce2a57ed0d65533dd230f63c7100ba2c
Aleksa Andjelic <23151811a@gmail.com>
2025-02-25 14:50:39 +0100

    Add skeleton layout for News page

--- apps/website/src/components/news/news.tsx
+++ apps/website/src/components/news/news.tsx
@@ -1,8 +1,9 @@
-import NewsPreview from "@components/news-preview/news-preview";
-import "./news.scss";
 import useLoader from "@better-router/use-loader";
-import newsPageLoader from "./news-page-loader";
 import LazyAwaitedList from "@components/lazy-loaded-list/lazy-awaited-list";
+import NewsPreview from "@components/news-preview/news-preview";
+import newsPageLoader from "./news-page-loader";
+import "./news.scss";
+import "../news-preview/news-preview.scss";
 
 export default function News() {
   const loaderData = useLoader<typeof newsPageLoader>();
@@ -10,7 +11,13 @@ export default function News() {
   return (
     <div className="news-page-container">
       <div className="articles-container">
-        <LazyAwaitedList data={loaderData} success="OK">
+        <LazyAwaitedList
+          data={loaderData}
+          success="OK"
+          skeleton={Array.from({ length: 9 }).map((_, i) => (
+            <div className="news-article-preview skeleton" key={"skeleton_" + i}></div>
+          ))}
+        >
           {(data) => <NewsPreview key={data.id} news={data} />}
         </LazyAwaitedList>
       </div>
