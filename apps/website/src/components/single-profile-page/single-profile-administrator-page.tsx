037fa91b5d06b33f79bcb9e62e96cc8eb74f5d51
Aleksa Andjelic <23151811a@gmail.com>
2025-03-06 15:29:57 +0100

    Translate SingleProfileAdministratorPage

--- apps/website/src/components/single-profile-page/single-profile-administrator-page.tsx
+++ apps/website/src/components/single-profile-page/single-profile-administrator-page.tsx
@@ -9,7 +9,7 @@ import "./single-profile-page.scss";
 import SubjectOverlay from "./subject-overlay";
 import { useTranslation } from "react-i18next";
 
-export default function SingleProfileITPage() {
+export default function SingleProfileNetworkAdminPage() {
   const loaderData = useLoader<typeof SingleProfilePageLoader>();
 
   const { t } = useTranslation();
@@ -59,67 +59,103 @@ export default function SingleProfileITPage() {
         <div className="image-container">
           <img
             src="/images/profiles/administrator-racunarskih-mreza.jpg"
-            alt="Placeholder"
+            alt="Administrator računarskih mreža"
           />
         </div>
 
         <div className="info">
-          <h1>{t("educationalProfiles.it.title")}</h1>
+          <h1>{t("educationalProfiles.administrator.title")}</h1>
 
-          <p>{t("educationalProfiles.it.descriptionOne")}</p>
+          <p>{t("educationalProfiles.administrator.descriptionOne")}</p>
 
-          <p>{t("educationalProfiles.it.descriptionTwo")}</p>
+          <p>{t("educationalProfiles.administrator.descriptionTwo")}</p>
         </div>
       </div>
 
       <div className="body">
         <section>
-          <h2> {t("educationalProfiles.it.program.title")}</h2>
+          <h2>{t("educationalProfiles.administrator.program.title")}</h2>
 
-          <p>{t("educationalProfiles.it.program.descriptionOne")}</p>
+          <p>{t("educationalProfiles.administrator.program.descriptionOne")}</p>
 
           <ul className="skills">
             <li>
-              <h2>{t("educationalProfiles.it.program.programming.title")}</h2>
+              <h2>
+                {t("educationalProfiles.administrator.program.network.title")}
+              </h2>
               <p>
-                {t("educationalProfiles.it.program.programming.description")}
+                {t(
+                  "educationalProfiles.administrator.program.network.description"
+                )}
               </p>
             </li>
 
             <li>
-              <h2>{t("educationalProfiles.it.program.web.title")}</h2>
-              <p>{t("educationalProfiles.it.program.web.description")}</p>
+              <h2>
+                {t("educationalProfiles.administrator.program.security.title")}
+              </h2>
+              <p>
+                {t(
+                  "educationalProfiles.administrator.program.security.description"
+                )}
+              </p>
             </li>
 
             <li>
-              <h2>{t("educationalProfiles.it.program.database.title")}</h2>
-              <p>{t("educationalProfiles.it.program.database.description")}</p>
+              <h2>
+                {t("educationalProfiles.administrator.program.hardware.title")}
+              </h2>
+              <p>
+                {t(
+                  "educationalProfiles.administrator.program.hardware.description"
+                )}
+              </p>
             </li>
 
             <li>
-              <h2>{t("educationalProfiles.it.program.network.title")}</h2>
-              <p>{t("educationalProfiles.it.program.network.description")}</p>
+              <h2>
+                {t(
+                  "educationalProfiles.administrator.program.operatingSystems.title"
+                )}
+              </h2>
+              <p>
+                {t(
+                  "educationalProfiles.administrator.program.operatingSystems.description"
+                )}
+              </p>
             </li>
 
             <li>
-              <h2>{t("educationalProfiles.it.program.hardware.title")}</h2>
-              <p>{t("educationalProfiles.it.program.hardware.description")}</p>
+              <h2>
+                {t("educationalProfiles.administrator.program.database.title")}
+              </h2>
+              <p>
+                {t(
+                  "educationalProfiles.administrator.program.database.description"
+                )}
+              </p>
             </li>
           </ul>
 
-          <p>{t("educationalProfiles.it.program.descriptionTwo")}</p>
+          <p>{t("educationalProfiles.administrator.program.descriptionTwo")}</p>
         </section>
 
         <section>
-          <h2>{t("educationalProfiles.it.knowledgeApplication.title")}</h2>
+          <h2>
+            {t("educationalProfiles.administrator.knowledgeApplication.title")}
+          </h2>
 
-          <p>{t("educationalProfiles.it.knowledgeApplication.description")}</p>
+          <p>
+            {t(
+              "educationalProfiles.administrator.knowledgeApplication.description"
+            )}
+          </p>
         </section>
 
         <section>
-          <h2>{t("educationalProfiles.it.target.title")}</h2>
+          <h2>{t("educationalProfiles.administrator.target.title")}</h2>
 
-          <p>{t("educationalProfiles.it.target.description")}</p>
+          <p>{t("educationalProfiles.administrator.target.description")}</p>
         </section>
       </div>
 
