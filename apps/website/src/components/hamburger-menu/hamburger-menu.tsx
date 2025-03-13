6b4f88a448c024c6b9a1bf4e8a5e0a9bb296dd69
Aleksa Andjelic <23151811a@gmail.com>
2025-03-14 00:11:11 +0100

    Add settings button to HamburgerMenu

--- apps/website/src/components/hamburger-menu/hamburger-menu.tsx
+++ apps/website/src/components/hamburger-menu/hamburger-menu.tsx
@@ -1,7 +1,10 @@
 import HamburgerNavigation from "@components/hamburger-navigation/hamburger-navigation";
-import { motion } from "motion/react";
-import { useTranslation } from "react-i18next";
 import "./hamburger-menu.scss";
+
+import Icon from "@components/icon/icon";
+import { useState, useEffect, useRef } from "react";
+import useOutsideClick from "@hooks/use-outside-click";
+import { useTranslation } from "react-i18next";
 import { useRevalidator } from "react-router";
 
 type HamburgerMenuProps = {
@@ -15,8 +18,48 @@ function HamburgerMenu({
   onRequestOpen,
   onRequestClose,
 }: HamburgerMenuProps) {
+  const [selectedTheme, setSelectedTheme] = useState<
+    "light" | "dark" | "system"
+  >("system");
+
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
   const { i18n } = useTranslation();
   const revalidator = useRevalidator();
+
+  const languageOptions = {
+    sr_lt: "Latinica",
+    sr_cr: "Ћирилица",
+    en: "English",
+  };
+
+  const currentLanguage = i18n.language as keyof typeof languageOptions;
+
+  const handleLanguageChange = (newLanguage: keyof typeof languageOptions) => {
+    i18n.changeLanguage(newLanguage);
+    revalidator.revalidate();
+  };
+
+  const popupRef = useRef<HTMLDivElement>(null);
+
+  useOutsideClick(popupRef, () => {
+    setIsPopupOpen(false);
+  });
+
+  useEffect(() => {
+    const theme = localStorage.getItem("theme");
+
+    if (theme) {
+      document.documentElement.dataset.theme = theme;
+      setSelectedTheme(theme as "light" | "dark");
+    } else {
+      if (!window.matchMedia) return;
+
+      const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+      const theme = mediaQuery.matches ? "dark" : "light";
+      setSelectedTheme(theme);
+      localStorage.setItem("theme", theme);
+    }
+  }, []);
 
   return (
     <>
@@ -46,22 +89,81 @@ function HamburgerMenu({
         </svg>
       </button>
 
-      <motion.button
-        animate={{
-          display: isHamburgerMenuOpen ? "block" : "none",
-          opacity: isHamburgerMenuOpen ? 1 : 0,
-        }}
-        onClick={() => {
-          i18n.changeLanguage(i18n.language === "sr" ? "en" : "sr");
-          revalidator.revalidate();
-        }}
-        className="hamburger-menu-language"
-        aria-hidden={!isHamburgerMenuOpen}
-        tabIndex={isHamburgerMenuOpen ? 0 : -1}
-        aria-label="Promeni jezik"
+      {isHamburgerMenuOpen && (
+        <button
+          onClick={() => {
+            setIsPopupOpen((prevState) => !prevState);
+          }}
+          className="settings-button-hamburger"
+          aria-label="Open settings"
+        >
+          <Icon
+            name="gear"
+            className={`gear ${isPopupOpen ? "active" : ""}`}
+          ></Icon>
+        </button>
+      )}
+
+      <div
+        ref={popupRef}
+        className={`settings-popup ${isPopupOpen ? "" : "closed"}`}
       >
-        {i18n.language === "sr" ? <p>Srpski</p> : <p>English</p>}
-      </motion.button>
+        <div className="language-options">
+          <button
+            onClick={() => handleLanguageChange("sr_lt")}
+            tabIndex={isPopupOpen ? 0 : -1}
+            className={`language-button ${
+              currentLanguage === "sr_lt" ? "active-language" : ""
+            }`}
+          >
+            {languageOptions.sr_lt}
+          </button>
+          <button
+            onClick={() => handleLanguageChange("sr_cr")}
+            tabIndex={isPopupOpen ? 0 : -1}
+            className={`language-button ${
+              currentLanguage === "sr_cr" ? "active-language" : ""
+            }`}
+          >
+            {languageOptions.sr_cr}
+          </button>
+          <button
+            onClick={() => handleLanguageChange("en")}
+            tabIndex={isPopupOpen ? 0 : -1}
+            className={`language-button ${
+              currentLanguage === "en" ? "active-language" : ""
+            }`}
+          >
+            {languageOptions.en}
+          </button>
+        </div>
+        <button
+          className="theme-button"
+          onClick={() => {
+            const newTheme = selectedTheme === "light" ? "dark" : "light";
+            setSelectedTheme(newTheme);
+
+            localStorage.setItem("theme", newTheme);
+            document.documentElement.dataset.theme = newTheme;
+          }}
+        >
+          <div
+            className={`theme-icons-container ${
+              selectedTheme === "dark" && "dark-theme-active"
+            }`}
+          >
+            <Icon
+              name="lightbulb"
+              className={`sun ${selectedTheme === "light" ? "active" : ""}`}
+            />
+
+            <Icon
+              name="moon"
+              className={`moon ${selectedTheme === "dark" ? "active" : ""}`}
+            />
+          </div>
+        </button>
+      </div>
 
       <HamburgerNavigation
         isMenuActive={isHamburgerMenuOpen}
