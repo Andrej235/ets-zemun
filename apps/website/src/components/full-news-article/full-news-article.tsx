ac37b0931936bc664e09735c2c08b9cf7bf25a4f
Andrej235 <andrej.nenadic06@gmail.com>
2025-05-26 17:51:07 +0200

    Remove image lazy loading from old website's full news article to fix a type error

--- apps/website/src/components/full-news-article/full-news-article.tsx
+++ apps/website/src/components/full-news-article/full-news-article.tsx
@@ -1,28 +1,12 @@
+import Async from "@/better-router/async";
 import useLoader from "@/better-router/use-loader";
+import { useRef } from "react";
 import fullNewsArticleLoader from "./full-news-article-loader";
 import "./full-news-article.scss";
-import Async from "@/better-router/async";
-import { useRef } from "react";
-import useLazyLoad from "@/hooks/use-lazy-load";
 
 export default function FullNewsArticle() {
   const loaderData = useLoader<typeof fullNewsArticleLoader>();
   const containerRef = useRef<HTMLDivElement>(null);
-
-  useLazyLoad(
-    loaderData.then((x) => (x.code === "200" ? x.content.images : null)),
-    (x) => {
-      if (!containerRef.current) return;
-
-      x.forEach((image) => {
-        const imageRef = containerRef.current!.querySelector(
-          `img#image-${image.id}`,
-        );
-
-        imageRef?.setAttribute("src", image.image);
-      });
-    },
-  );
 
   return (
     <Async await={loaderData}>
