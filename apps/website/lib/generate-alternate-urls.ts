4e18265e9e60cd8998efcdc6ae6da14be4bd5850
Andrej235 <andrej.nenadic06@gmail.com>
2025-05-30 17:52:15 +0200

    Change the metadata's canonical and alternate links

--- apps/website/lib/generate-alternate-urls.ts
+++ apps/website/lib/generate-alternate-urls.ts
@@ -6,11 +6,10 @@ export default function generateAlternateUrls(
 ): Metadata["alternates"] {
   const baseUrl = process.env.NEXT_PUBLIC_WEBSITE_URL;
   const otherLanguages: Record<string, string> = {
-    sr: `${baseUrl}/srb/${extension}`,
+    "sr-Cyrl": `${baseUrl}/srb/${extension}`,
     "sr-Latn": `${baseUrl}/lat/${extension}`,
     en: `${baseUrl}/eng/${extension}`,
   };
-  delete otherLanguages[locale === "lat" ? "sr-Latn" : locale];
 
   return {
     canonical: `${baseUrl}/${locale}/${extension}`,
