24c2e72d6a3369cb478b5d3506910a2dfdca4907
andjelic-a <23151811a@gmail.com>
2025-07-03 17:51:28 +0000

    Fix error

--- apps/website/components/enrollment/enrollment.tsx
+++ apps/website/components/enrollment/enrollment.tsx
@@ -105,7 +105,7 @@ export default async function Enrollment({
             ))}
           </tbody>
         </table>
-      </div>>
+      </div>
 
       <div className="call-to-action">
         <p>{t("enrollment.callToAction.tagline")}</p>
