6cec3be2d81e2c78b0cba8d608e4efe70b95b445
Andrej235 <andrej.nenadic06@gmail.com>
2025-08-05 18:48:35 +0200

    Create a nextjs endpoint for converting pdf files into a more basic version of exams, remove most of the ui from exams page

---
+++ apps/admin/app/api/upload-pdf/route.ts
@@ -0,0 +1,73 @@
+import pdfParse from "pdf-parse";
+
+export async function POST(req: Request) {
+  try {
+    const formData = await req.formData();
+    const file = formData.get("file");
+
+    if (!(file instanceof File)) {
+      return new Response(JSON.stringify({ error: "No file provided" }), {
+        status: 400,
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+
+    const buffer = Buffer.from(await file.arrayBuffer());
+
+    const result = await pdfParse(buffer);
+
+    return new Response(
+      JSON.stringify({
+        exams: extractExams(result.text),
+      }),
+      { status: 200, headers: { "Content-Type": "application/json" } },
+    );
+  } catch (error) {
+    return new Response(JSON.stringify({ error: (error as Error).message }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+}
+
+type Exam = {
+  subject: string;
+  commission: string;
+  date: string;
+  startTime: string;
+  cabinet: string;
+};
+
+function extractExams(data: string): Exam[] {
+  const pages = data
+    .split("\n\n")
+    .map((x) => x.substring(x.indexOf("КАБИНЕТ") + 7).trim())
+    .filter((x) => x);
+
+  const exams = pages.join(" ").split("\n").join(" ") + " ";
+
+  function extractDates(input: string) {
+    const regex =
+      /^(.*?)(((1|2|3)\..+?){1,3})([(\d{1,2}\.\d{1,2}\.\d{4}\.) ]+?)(\d{1,2}:\d{2})(.+?) /;
+
+    const exams: Exam[] = [];
+
+    while (input.length > 0) {
+      input = input.replace(regex, (x, ...y) => {
+        const exam = {
+          subject: y[0],
+          commission: y[1],
+          date: y[4],
+          startTime: y[5],
+          cabinet: y[6],
+        };
+        exams.push(exam);
+        return "";
+      });
+    }
+
+    return exams;
+  }
+
+  return extractDates(exams);
+}
