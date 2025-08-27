3c28ffd9ccab2a7ab583092fdea9a4c57641c1b1
andjelic-a <23151811a@gmail.com>
2025-08-27 20:01:57 +0000

    Add important alert to about page

--- apps/website/components/about/about.tsx
+++ apps/website/components/about/about.tsx
@@ -95,6 +95,27 @@ export default async function About({
         </div>
       </section>
 
+      <section className="important-alerts">
+        <div className="announcement">
+                <h1>{t("enrollment.announcement.header")}</h1>
+                <p>{t("enrollment.announcement.announcement.0")}</p>
+                <h3>{t("enrollment.announcement.listHeader")}</h3>
+                <ul>
+                  <li>{t("enrollment.announcement.list.0")}</li>
+                  <li>{t("enrollment.announcement.list.1")}</li>
+                </ul>
+                <p>{t("enrollment.announcement.text")}</p>
+        
+                <Image
+                  src="/images/announcement-table.webp"
+                  alt={t("enrollment.announcement.tableAlt")}
+                  width={1200}
+                  height={1600}
+                  unoptimized
+                />
+              </div>
+      </section>
+
       <section className="school-preview-cards-container">
         <SchoolPreviewCard
           count={75}
